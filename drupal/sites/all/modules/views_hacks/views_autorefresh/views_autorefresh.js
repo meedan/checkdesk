c733b27f12ad6e196f1d79fda045c29607246d00
Karim Ratib <karim.ratib@gmail.com>
2012-08-14 17:40:22 -0700

    More work on auto-refreshing views

--- drupal/sites/all/modules/views_hacks/views_autorefresh/views_autorefresh.js
+++ drupal/sites/all/modules/views_hacks/views_autorefresh/views_autorefresh.js
@@ -31,16 +31,16 @@ Drupal.behaviors.views_autorefresh = {
             // Set a reference that will work in subsequent calls.
             var target = this;
             $('select,input,textarea', target)
-            .click(function () {
-              if (Drupal.settings.views_autorefresh[settings.view_name].timer) {
-                clearTimeout(Drupal.settings.views_autorefresh[settings.view_name].timer);
-              }
-            })
-            .change(function () {
-              if (Drupal.settings.views_autorefresh[settings.view_name].timer) {
-                clearTimeout(Drupal.settings.views_autorefresh[settings.view_name].timer);
-              }
-            });
+              .click(function () {
+                if (Drupal.settings.views_autorefresh[settings.view_name].timer) {
+                  clearTimeout(Drupal.settings.views_autorefresh[settings.view_name].timer);
+                }
+              })
+              .change(function () {
+                if (Drupal.settings.views_autorefresh[settings.view_name].timer) {
+                  clearTimeout(Drupal.settings.views_autorefresh[settings.view_name].timer);
+                }
+              });
             $(this)
               .addClass('views-autorefresh-processed')
               // Process pager, tablesort, and attachment summary links.
@@ -60,17 +60,17 @@ Drupal.behaviors.views_autorefresh = {
                 );
 
                 $.extend(viewData, Drupal.Views.parseViewArgs($(this).attr('href'), settings.view_base_path));
-                $(this).addClass('views-throbbing');
-                var base = view;
+
+                // Setup the click response with Drupal.ajax.
                 var element_settings = {};
                 element_settings.url = ajax_path;
                 element_settings.event = 'click';
-                element_settings.selector = base;
+                element_settings.selector = view;
                 element_settings.submit = viewData;
-                var ajax = new Drupal.ajax(base, this, element_settings);
+                var ajax = new Drupal.ajax(view, this, element_settings);
 
                 // Activate refresh timer.
-                Drupal.views_autorefresh.timer(settings.view_name, anchor);
+                Drupal.views_autorefresh.timer(settings, anchor, target);
               }); // .each function () {
         }); // $view.filter().each
       });
@@ -78,12 +78,47 @@ Drupal.behaviors.views_autorefresh = {
   }
 }
 
-Drupal.views_autorefresh.timer = function(view_name, anchor) {
-  Drupal.settings.views_autorefresh[view_name].timer = setTimeout(function() {
-    clearTimeout(Drupal.settings.views_autorefresh[view_name].timer);
-    $(anchor).click();
-  }, Drupal.settings.views_autorefresh[view_name].interval);
+Drupal.views_autorefresh.timer = function(settings, anchor, target) {
+  Drupal.settings.views_autorefresh[settings.view_name].timer = setTimeout(function() {
+    clearTimeout(Drupal.settings.views_autorefresh[settings.view_name].timer);
+
+    // Handle ping path.
+    var ping_base_path;
+    if (Drupal.settings.views_autorefresh[settings.view_name].ping) {
+      ping_base_path = Drupal.settings.views_autorefresh[settings.view_name].ping.ping_base_path;
+    }
+
+    // If there's a ping URL, hit it first.
+    if (ping_base_path) {
+      $.ajax({
+        url: Drupal.settings.basePath + ping_base_path,
+        data: {
+          timestamp: Drupal.settings.views_autorefresh[settings.view_name].timestamp
+        },
+        success: function(response) {
+          if (response.pong && parseInt(response.pong) > 0) {
+            $(target).trigger('autorefresh.ping', parseInt(response.pong));
+            $(anchor).trigger('click');
+          }
+          else {
+            Drupal.views_autorefresh.timer(settings, anchor, target);
+          }
+        },
+        error: function(xhr) {},
+        dataType: 'json',
+      });
+    }
+    else {
+      $(anchor).trigger('click');
+    }
+  }, Drupal.settings.views_autorefresh[settings.view_name].interval);
+}
+
+Drupal.ajax.prototype.commands.viewsAutoRefreshTriggerUpdate = function (ajax, response, status) {
+  var $target = $('.view-dom-id-' + response.view_dom_id);
+  $target.trigger('autorefresh.update', response.timestamp);
 }
 
 // END jQuery
 })(jQuery);
+
