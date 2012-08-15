bc198ddb79cd243e773b011519ffbddb55feae8b
melsawy <melsawy.be7eri@gmail.com>
2012-08-15 21:11:33 +0200

    intial import -- this module to handle oembed for checkdesk

---
+++ drupal/sites/all/modules/checkdesk/meedan_oembed/theme/oembed-facebook.tpl.php
@@ -0,0 +1,15 @@
+<?php if ($author_img) : ?>
+  <div class="author_img"><?php print $author_img ?></div>
+<?php endif ?>
+<?php if ($author_name) : ?>
+  <div class="author_name"><?php print $author_name ?></div>
+<?php endif ?>
+<?php if ($content) : ?>
+  <div class="content"><?php print $content ?></div>
+<?php endif ?>
+<?php if ($favicon_link) : ?>
+  <div class="favicon"><?php print $favicon_link ?></div>
+<?php endif ?>
+<?php if ($domain_link) : ?>
+  <div class="domain"><?php print $domain_link ?></div>
+<?php endif ?>
---
+++ drupal/sites/all/modules/checkdesk/meedan_oembed/theme/oembed-generic.tpl.php
@@ -0,0 +1,9 @@
+<?php if ($title_link) : ?>
+  <div class="title"><?php print $title_link ?></div>
+<?php endif ?>
+<?php if ($favicon_link) : ?>
+  <div class="favicon"><?php print $favicon_link ?></div>
+<?php endif ?>
+<?php if ($domain_link) : ?>
+  <div class="domain"><?php print $domain_link ?></div>
+<?php endif ?>
