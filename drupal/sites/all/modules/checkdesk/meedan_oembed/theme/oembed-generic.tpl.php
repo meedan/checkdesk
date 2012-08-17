00ea11b68ed0b54521fc034b4b9fafb48911fc3a
melsawy <melsawy.be7eri@gmail.com>
2012-08-17 08:47:46 +0200

    re-define theme function to match embedly response

--- drupal/sites/all/modules/checkdesk/meedan_oembed/theme/oembed-generic.tpl.php
+++ drupal/sites/all/modules/checkdesk/meedan_oembed/theme/oembed-generic.tpl.php
@@ -4,6 +4,13 @@
 <?php if ($favicon_link) : ?>
   <div class="favicon"><?php print $favicon_link ?></div>
 <?php endif ?>
+<?php if ($thumbnail) : ?>
+  <div class="thumbnail"><?php print $thumbnail ?></div>
+<?php endif ?>
+<?php if ($content) : ?>
+  <div class="content"><?php print $content ?></div>
+<?php endif ?>
+
 <?php if ($domain_link) : ?>
   <div class="domain"><?php print $domain_link ?></div>
 <?php endif ?>
