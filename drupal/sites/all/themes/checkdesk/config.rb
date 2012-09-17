95af2307115286841559a0917d6fdb771cf4b14d
Karim Ratib <karim.ratib@gmail.com>
2012-09-17 10:32:54 -0700

    Adding bowerbird theme as submodule

---
+++ drupal/sites/all/themes/checkdesk/config.rb
@@ -0,0 +1,8 @@
+load '../bowerbird/framework'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
