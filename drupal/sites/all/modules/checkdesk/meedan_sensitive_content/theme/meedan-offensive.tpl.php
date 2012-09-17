41d3ec26ad0c3c1ca736803832ec91fdf4f5d29f
melsawy <melsawy.be7eri@gmail.com>
2012-09-17 09:17:22 +0200

    Add template to content sensitive display

---
+++ drupal/sites/all/modules/checkdesk/meedan_sensitive_content/theme/meedan-offensive.tpl.php
@@ -0,0 +1,8 @@
+
+<div class="offensive-content">
+<div class="offensive-notification offensive-notification-<?php print $meedan_offensive_nid; ?>">
+<div class="offensive-notification-text">
+<p><?php print $meedan_offensive_text; ?></p>
+<p><?php print $meedan_offensive_link; ?></p>
+</div>
+</div>
