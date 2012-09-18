95cbb9d45fc6a02b963ba44eabc281ff86e760e1
melsawy <melsawy.be7eri@gmail.com>
2012-09-18 05:13:34 +0200

    Add settings to user profile to enable/disable offensive content, add new permission and apply content global setting to node creation

---
+++ drupal/sites/all/modules/checkdesk/meedan_sensitive_content/js/meedan_offensive.js
@@ -0,0 +1,5 @@
+
+function meedan_display_offensive(nid) {
+  jQuery('div.offensive-notification-'+nid).hide();
+  jQuery('div.offensive-item-'+nid).show();
+}
--- drupal/sites/all/modules/checkdesk/meedan_sensitive_content/theme/meedan-offensive.tpl.php
+++ drupal/sites/all/modules/checkdesk/meedan_sensitive_content/theme/meedan-offensive.tpl.php
@@ -1,5 +1,4 @@
 
-<div class="offensive-content">
 <div class="offensive-notification offensive-notification-<?php print $meedan_offensive_nid; ?>">
 <div class="offensive-notification-text">
 <p><?php print $meedan_offensive_text; ?></p>
