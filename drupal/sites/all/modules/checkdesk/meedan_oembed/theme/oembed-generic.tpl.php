a41d2a82e3ddbf52f2a8cab331d4c835bac0ccf6
Karim Ratib <karim.ratib@gmail.com>
2012-09-26 13:30:48 -0700

    Fix media embedding

--- drupal/sites/all/modules/checkdesk/meedan_oembed/theme/oembed-generic.tpl.php
+++ drupal/sites/all/modules/checkdesk/meedan_oembed/theme/oembed-generic.tpl.php
@@ -1,16 +1,15 @@
 <?php if ($title_link) : ?>
   <div class="title"><?php print $title_link ?></div>
 <?php endif ?>
-<?php if ($favicon_link) : ?>
-  <div class="favicon"><?php print $favicon_link ?></div>
-<?php endif ?>
 <?php if ($thumbnail) : ?>
   <div class="thumbnail"><?php print $thumbnail ?></div>
 <?php endif ?>
 <?php if ($content) : ?>
   <div class="content"><?php print $content ?></div>
 <?php endif ?>
-
+<?php if ($favicon_link) : ?>
+  <div class="favicon"><?php print $favicon_link ?></div>
+<?php endif ?>
 <?php if ($domain_link) : ?>
   <div class="domain"><?php print $domain_link ?></div>
 <?php endif ?>
