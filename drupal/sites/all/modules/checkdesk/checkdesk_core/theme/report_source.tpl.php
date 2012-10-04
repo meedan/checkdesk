4b62ee28477f6d4281c5b74e505803d6acc3dbaa
Karim Ratib <karim.ratib@gmail.com>
2012-10-04 16:24:49 -0700

    Add report source info

---
+++ drupal/sites/all/modules/checkdesk/checkdesk_core/theme/report_source.tpl.php
@@ -0,0 +1,4 @@
+<div class="checkdesk-report-source">
+<div class="checkdesk-report-source-url"><?php print $source_url ?></div>
+<div class="checkdesk-report-source-author"><?php print $source_author ?></div>
+</div>
