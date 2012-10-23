ab9a33cc7c5ad23c3f1323da919a7806dbdefdce
James Andres <james.andres@gmail.com>
2012-10-23 13:20:41 +0200

    Add story status blocks
    Conflicts:
    	drupal/sites/all/modules/checkdesk/checkdesk_core_feature/checkdesk_core_feature.features.menu_links.inc

---
+++ drupal/sites/all/modules/checkdesk/checkdesk_core/theme/story_blogger.tpl.php
@@ -0,0 +1,6 @@
+<div class="story-blogger">
+Current blogger
+<?php print $blogger_picture ?><?php print $blogger_name ?>
+<div class="story-blogger-status <?php print $blogger_status_class ?>"></div>
+<div class="story-blogger-text"><?php print $blogger_status_text ?></div>
+</div>
---
+++ drupal/sites/all/modules/checkdesk/checkdesk_core/theme/story_status.tpl.php
@@ -0,0 +1,4 @@
+<div class="story-status">
+<div><?php print $story_status; ?></div>
+<div><?php print $story_context; ?></div>
+</div>
