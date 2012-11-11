0400c279fc4204272fe367a01e97b292c009c6c8
designbysoil <designbysoil@gmail.com>
2012-11-11 02:48:45 +0000

    Removed span tags from the flag drop down menu links to allow hover effect.

--- drupal/sites/all/themes/checkdesk/templates/flag.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/flag.tpl.php
@@ -30,13 +30,11 @@
  * advanced theming you may have to remove all the whitespace.
  */
 ?>
-<span class="<?php print $flag_wrapper_classes; ?>">
-  <?php if ($link_href): ?>
-    <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow"><?php print $link_text; ?></a>
-  <?php else: ?>
-    <span class="<?php print $flag_classes ?>"><?php print $link_text; ?></span>
-  <?php endif; ?>
-  <?php if ($after_flagging): ?>
-    <?php drupal_set_message($message_text, 'warning'); ?>
-  <?php endif; ?>
-</span>
+<?php if ($link_href): ?>
+  <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow"><?php print $link_text; ?></a>
+<?php else: ?>
+  <?php print $link_text; ?>
+<?php endif; ?>
+<?php if ($after_flagging): ?>
+  <?php drupal_set_message($message_text, 'warning'); ?>
+<?php endif; ?>
