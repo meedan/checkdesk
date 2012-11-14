6943bcfc42aefaf7a388c2520d0ee1524479e8a9
Chris Blow <unthinkingly@gmail.com>
2012-11-13 14:39:25 -1000

    move Compass config.rb and add Guard support

---
+++ drupal/config.rb
@@ -0,0 +1,10 @@
+require "susy"
+load "sites/all/themes/bowerbird/framework"
+
+# Set this to the root of your project when deployed:
+http_path = "sites/all/themes/checkdesk"
+css_dir = "sites/all/themes/checkdesk/assets/css"
+sass_dir = "sites/all/themes/checkdesk/assets/scss"
+images_dir = "sites/all/themes/checkdesk/assets/img"
+javascripts_dir = "sites/all/themes/checkdesk/assets/js"
+fonts_dir = "sites/all/themes/checkdesk/assets/fonts"
