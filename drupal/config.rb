86956c998499e966c186cd131e496ed1a767304e
Chris Blow <unthinkingly@gmail.com>
2012-11-21 19:35:35 -1000

    update font-icon paths

--- drupal/config.rb
+++ drupal/config.rb
@@ -1,11 +1,13 @@
+require "susy" # how to require this in bowerbird?
 load "sites/all/themes/bowerbird/framework"
 
-require "susy" # how to require this in bowerbird?
-
 # Set this to the root of your project when deployed:
-http_path = "sites/all/themes/checkdesk"
-css_dir = "sites/all/themes/checkdesk/assets/css"
-sass_dir = "sites/all/themes/checkdesk/assets/scss"
-images_dir = "sites/all/themes/checkdesk/assets/img"
+http_path = "/"
+css_dir 				= "sites/all/themes/checkdesk/assets/css"
+sass_dir 				= "sites/all/themes/checkdesk/assets/scss"
+images_dir 			= "sites/all/themes/checkdesk/assets/img"
 javascripts_dir = "sites/all/themes/checkdesk/assets/js"
-fonts_dir = "sites/all/themes/checkdesk/assets/fonts"
+font_dir 				= "sites/all/themes/checkdesk/assets/fonts"
+
+http_fonts_path 	= "/sites/all/themes/checkdesk/assets/fonts"
+relative_assets = false
