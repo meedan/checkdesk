5ea2aa088246d9a2581a53bc1ef38f2782123504
designbysoil <designbysoil@gmail.com>
2012-11-25 17:10:54 +0000

    Trying to correctly override ctools modal markup.

--- drupal/sites/all/modules/custom/checkdesk_core/js/checkdesk_core.js
+++ drupal/sites/all/modules/custom/checkdesk_core/js/checkdesk_core.js
@@ -1,64 +1,42 @@
-(function ($) {
+/**
+* Provide the HTML to create the modal dialog.
+*/
+Drupal.theme.prototype.CToolsSampleModal = function () {
+  var html = '';
 
-  Drupal.behaviors.checkdesk = {
-    attach: function (context, settings) {
-      $('.draggable', context).draggable({
-        revert: 'invalid',
-        zIndex: 3000,
-      });
-      $('.droppable', context).droppable({
-        hoverClass: 'drop-hover',
-        accept: '.draggable',  
-        drop: function(event, ui) {
-          $(ui.draggable).hide();
-          // Retrieve the Views information from the DOM.
-          var data = $(ui.draggable).parent().parent().parent().data('views');
-          // Insert the report URL into the textarea of the post body.
-          $('textarea', this).insertAtCaret("\n" + data.droppable_ref + "\n");
-        },
-      });
-      // Attach the Views results to each correspoknding row in the DOM.
-      var i=0;
-      $('.view-desk-reports .view-content', context).children().each(function() {
-        $(this).data('views', settings.checkdesk.reports[i++]);
-      });
-    }
-  };
+  html += '<div id="ctools-modal" class="popups-box">';
+  html += '  <div class="ctools-modal-content ctools-sample-modal-content">';
+  html += '    <table cellpadding="0" cellspacing="0" id="ctools-face-table">';
+  html += '      <tr>';
+  html += '        <td class="popups-tl popups-border"></td>';
+  html += '        <td class="popups-t popups-border"></td>';
+  html += '        <td class="popups-tr popups-border"></td>';
+  html += '      </tr>';
+  html += '      <tr>';
+  html += '        <td class="popups-cl popups-border"></td>';
+  html += '        <td class="popups-c" valign="top">';
+  html += '          <div class="popups-container">';
+  html += '            <div class="modal-header popups-title">';
+  html += '              <span id="modal-title" class="modal-title"></span>';
+  html += '              <span class="popups-close"><a class="close" href="#">' + Drupal.CTools.Modal.currentSettings.closeText + '</a></span>';
+  html += '              <div class="clear-block"></div>';
+  html += '            </div>';
+  html += '            <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+  html += '            <div class="popups-buttons"></div>'; //Maybe someday add the option for some specific buttons.
+  html += '            <div class="popups-footer"></div>'; //Maybe someday add some footer.
+  html += '          </div>';
+  html += '        </td>';
+  html += '        <td class="popups-cr popups-border"></td>';
+  html += '      </tr>';
+  html += '      <tr>';
+  html += '        <td class="popups-bl popups-border"></td>';
+  html += '        <td class="popups-b popups-border"></td>';
+  html += '        <td class="popups-br popups-border"></td>';
+  html += '      </tr>';
+  html += '    </table>';
+  html += '  </div>';
+  html += '</div>';
 
-  Drupal.behaviors.checkdeskModal = {
-  attach: function (context){
+  return html;
 
-    // Make modal window height scaled automatically.
-    $('.ctools-modal-content, #modal-content', context).height('auto');
-
-    // Position code lifted from http://www.quirksmode.org/viewport/compatibility.html
-    if (self.pageYOffset) { // all except Explorer
-      var wt = self.pageYOffset;
-    }
-    else if (document.documentElement && document.documentElement.scrollTop) { // Explorer 6 Strict
-      var wt = document.documentElement.scrollTop;
-    }
-    else if (document.body) { // all other Explorers
-      var wt = document.body.scrollTop;
-    }
-
-    // Fix CTools bug: calculate correct 'top' value.
-    var mdcTop = wt + ( $(window).height() / 2 ) - ($('#modalContent', context).outerHeight() / 2);
-    $('#modalContent', context).css({top: mdcTop + 'px'});
-  }
 }
-
-  Drupal.theme.prototype.checkdesk_modal_style = {
-    attach: function (context){ 
-    var html = '';
-    html += '<div id="ctools-modal-checkdesk" class="popups-box">';
-    html += '  <div class="ctools-modal-content ctools-modal-happy-modal-content">';
-    html += '    <span class="popups-close"><a class="close" href="#"></a></span>';
-    html += '    <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
-    html += '  </div>';
-    html += '</div>';
-    return html;
-  }
-}
-
-})(jQuery);
