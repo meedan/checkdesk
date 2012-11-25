3bcc4143b5fc2f74f3ee6f660e3cb5737eabb825
designbysoil <designbysoil@gmail.com>
2012-11-25 17:34:24 +0000

    Adding proper modal markup.

--- drupal/sites/all/modules/custom/checkdesk_core/js/checkdesk_core.js
+++ drupal/sites/all/modules/custom/checkdesk_core/js/checkdesk_core.js
@@ -4,37 +4,23 @@
 Drupal.theme.prototype.CToolsSampleModal = function () {
   var html = '';
 
-  html += '<div id="ctools-modal" class="popups-box">';
-  html += '  <div class="ctools-modal-content ctools-sample-modal-content">';
-  html += '    <table cellpadding="0" cellspacing="0" id="ctools-face-table">';
-  html += '      <tr>';
-  html += '        <td class="popups-tl popups-border"></td>';
-  html += '        <td class="popups-t popups-border"></td>';
-  html += '        <td class="popups-tr popups-border"></td>';
-  html += '      </tr>';
-  html += '      <tr>';
-  html += '        <td class="popups-cl popups-border"></td>';
-  html += '        <td class="popups-c" valign="top">';
-  html += '          <div class="popups-container">';
-  html += '            <div class="modal-header popups-title">';
-  html += '              <span id="modal-title" class="modal-title"></span>';
-  html += '              <span class="popups-close"><a class="close" href="#">' + Drupal.CTools.Modal.currentSettings.closeText + '</a></span>';
-  html += '              <div class="clear-block"></div>';
-  html += '            </div>';
-  html += '            <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
-  html += '            <div class="popups-buttons"></div>'; //Maybe someday add the option for some specific buttons.
-  html += '            <div class="popups-footer"></div>'; //Maybe someday add some footer.
-  html += '          </div>';
-  html += '        </td>';
-  html += '        <td class="popups-cr popups-border"></td>';
-  html += '      </tr>';
-  html += '      <tr>';
-  html += '        <td class="popups-bl popups-border"></td>';
-  html += '        <td class="popups-b popups-border"></td>';
-  html += '        <td class="popups-br popups-border"></td>';
-  html += '      </tr>';
-  html += '    </table>';
+
+  html += '<div id="ctools-modal">';
+  html += '<div class="ctools-modal-content ctools-sample-modal-content">';
+  html += '<div class="modal">';
+  html += '  <div class="modal-header">';
+  html += '   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>';
+  html += '   <h3><span id="modal-title" class="modal-title"></span></h3>';
   html += '  </div>';
+  html += ' <div class="modal-body">';
+  html += '   <p><div id="modal-content" class="modal-content popups-body"></div></p>';
+  html += ' </div>';
+  html += ' <div class="modal-footer">';
+  html += '   <a href="#" class="btn">Close</a>';
+  html += '   <a href="#" class="btn btn-primary">Save changes</a>';
+  html += ' </div>';
+  html += '</div>';
+  html += '</div>';
   html += '</div>';
 
   return html;
