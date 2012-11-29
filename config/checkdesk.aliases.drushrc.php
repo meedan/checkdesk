97ae15347d731becbd6939a0b9c3c04b11fa694c
Karim Ratib <karim.ratib@gmail.com>
2012-11-29 10:48:28 -0800

    Adding drush alias

---
+++ config/checkdesk.aliases.drushrc.php
@@ -0,0 +1,8 @@
+<?php
+$aliases['dev'] = array(
+  'uri' => 'http://dev.checkdesk.meedan.net',
+  'root' => '/var/www/checkdesk.dev/current/drupal',
+  'remote-host' => 'meedan.net',
+  'ssh-options' => '-p8114',
+);
+
