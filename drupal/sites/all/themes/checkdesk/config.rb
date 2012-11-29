f91054da5e39a2a7d5dd59dcae5cce513465b74f
Chris Blow <unthinkingly@gmail.com>
2012-11-29 00:38:07 -1000

    forms, user, tabs, spec, report, remove sass-cache

--- drupal/sites/all/themes/checkdesk/config.rb
+++ drupal/sites/all/themes/checkdesk/config.rb
@@ -1,29 +1,23 @@
 require "susy" # how to require this in bowerbird?
+require "logger" # custom logger with pass/fail
+load "../bowerbird/framework" # is this the right way to do this?
 
-load "sites/all/themes/bowerbird/framework"
-debug_info = 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir 				= "sites/all/themes/checkdesk/assets/css"
-sass_dir 				= "sites/all/themes/checkdesk/assets/scss"
-images_dir 			= "sites/all/themes/checkdesk/assets/img"
-javascripts_dir = "sites/all/themes/checkdesk/assets/js"
+css_dir 				= "assets/css"
+sass_dir 				= "assets/scss"
+images_dir 			= "assets/img"
+javascripts_dir = "assets/js"
 
 # icon webfonts from bowerbird (by contrast typography fonts are from google)
-font_dir 				= "/sites/all/themes/bowerbird/framework/fonts"
+font_dir 				= ".../bowerbird/framework/fonts"
 http_fonts_path = "/sites/all/themes/bowerbird/framework/fonts"
 relative_assets = false
 
-# module Sass::Script::Functions
-#   def reverse(string)
-#     assert_type string, :String
-#     Sass::Script::String.new(string.value.reverse)
-#   end
-
-#   def random_color
-#     colors = ["#FF628C", "#3AD900", "#0088FF", "#80FFC2", "#FFDD00", "#FF9D00"]
-#     arr = colors.dup
-    
-#     Sass::Script::String.new(colors.collect { arr.slice!(rand(arr.length)) }.first);
-#   end
-# end
+module Sass::Script::Functions
+  def random_color
+    colors = ["#FF628C", "#3AD900", "#0088FF", "#80FFC2", "#FFDD00", "#FF9D00"]
+    arr = colors.dup    
+    Sass::Script::String.new(colors.collect { arr.slice!(rand(arr.length)) }.first);
+  end
+end
