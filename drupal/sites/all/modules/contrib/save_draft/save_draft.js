05e486a181c5f6f821fc42430df96ba28cdf3f77
Karim Ratib <karim.ratib@gmail.com>
2012-12-06 18:10:34 -0800

    Enable save draft button per content type

--- drupal/sites/all/modules/contrib/save_draft/save_draft.js
+++ drupal/sites/all/modules/contrib/save_draft/save_draft.js
@@ -1,10 +1,12 @@
 (function ($) {
 
 /**
- * Remove publication status from the "Content promotion options" vertical tab.
+ * Show summaries of selected options within tabs.
  */
 Drupal.behaviors.saveDraftFieldsetSummaries = {
   attach: function (context) {
+    // Remove publication status from the "Content promotion options" vertical
+    // tab.
     $('fieldset.node-promotion-options', context).drupalSetSummary(function (context) {
       var vals = [];
 
@@ -17,6 +19,22 @@ Drupal.behaviors.saveDraftFieldsetSummaries = {
       else {
         return Drupal.t('Not promoted');
       }
+    });
+
+    // Display save draft settings summary on the node options fieldet.
+    $('fieldset#edit-save-draft', context).drupalSetSummary(function (context) {
+      var vals = [];
+
+      // Add summary text for each checked option.
+      $('input:checked', context).next('label').each(function() {
+        vals.push(Drupal.checkPlain($(this).text()));
+      });
+
+      // For the case of the "Enabled" checkbox also handle the disabled state.
+      if (!$('#edit-save-draft-enabled', context).is(':checked')) {
+        vals.unshift(Drupal.t('Disabled'));
+      }
+      return vals.join(', ');
     });
   }
 };
