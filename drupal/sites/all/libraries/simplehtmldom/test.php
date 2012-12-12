26f3150776d804e15ae58127d9766ee70e42acfd
melsawy <melsawy.be7eri@gmail.com>
2012-12-12 09:10:16 +0200

    Use google getFavicon service http://getfavicon.appspot.com/ instead of simplehtmldom library

--- drupal/sites/all/libraries/simplehtmldom/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// example of how to use basic selector to retrieve HTML contents
-include('simple_html_dom.php');
- 
-// get DOM from URL or file
-$html = file_get_html('http://www.facebook.com/AhmadMAbdullah/posts/10151075975752806');
-print_r($html);
-
