7b925ef5c360c674ec8113e7d3c91ebbb93da84a
Caio SBA <caiosba@gmail.com>
2012-12-27 17:57:22 -0200

    Ticket #925: Implementing a custom parser for Al-Masry Al-Youm

---
+++ drupal/sites/all/modules/meedan/meedan_oembed/theme/oembed-almasryalyoum.tpl.php
@@ -0,0 +1,9 @@
+<?php if ($content) : ?>
+  <div class="content"><?php print $content ?></div>
+<?php endif ?>
+<?php if ($favicon_link) : ?>
+  <div class="favicon"><?php print $favicon_link ?></div>
+<?php endif ?>
+<?php if ($domain_link) : ?>
+  <div class="domain"><?php print $domain_link ?></div>
+<?php endif ?>
