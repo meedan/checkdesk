5ff507db2cd9470e23d62a7ea5a9cec8c89c54a8
Caio SBA <caiosba@gmail.com>
2013-01-13 22:49:37 -0200

    Tickets #975 and #934: Refactoring Livefyre comments (much simpler and optimized now) and solving bug with views auto refresh

--- drupal/sites/all/modules/custom/checkdesk_core/autorefresh/liveblog.php
+++ drupal/sites/all/modules/custom/checkdesk_core/autorefresh/liveblog.php
@@ -1,15 +1,19 @@
 <?php
-
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past (HTTP 1.0)
 require('../../../../../../sites/default/settings.php');
 
+// Connect to the database
+global $databases;
 $p = $databases['default']['default'];
-$mysql = mysql_pconnect($p['host'], $p['username'], $p['password']);
+$mysql = mysql_pconnect($p['host'], $p['username'], @$p['password']);
 if (!$mysql) die('Could not connect to database server.');
 mysql_set_charset('utf8', $mysql);
 if (!mysql_select_db($p['database'], $mysql)) die('Could not select database.');
  
 $timestamp = mysql_real_escape_string($_REQUEST['timestamp']);
 $type = mysql_real_escape_string($_REQUEST['type']);
+
 $sql = "SELECT COUNT(nid) FROM node WHERE type='" . $type .  "' AND created > " . $timestamp;
 if (!empty($_REQUEST['uid'])) {
   $uid = mysql_real_escape_string($_REQUEST['uid']);
