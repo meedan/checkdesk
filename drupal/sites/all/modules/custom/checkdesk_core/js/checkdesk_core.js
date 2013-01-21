15bbe79c369ebedec7036ed44b7974a1a3147660
Caio SBA <caiosba@gmail.com>
2013-01-21 21:08:20 -0200

    Ticket #981: Toggle filters on reports sidebar on creating update

--- drupal/sites/all/modules/custom/checkdesk_core/js/checkdesk_core.js
+++ drupal/sites/all/modules/custom/checkdesk_core/js/checkdesk_core.js
@@ -52,8 +52,13 @@
         $(this).data('views', settings.checkdesk.reports[i++]);
       });
       // Restrict thumbnail width to 220
-      jQuery('.view-desk-reports .view-content').find('.thumbnail img').width(220);
-
+      $('.view-desk-reports .view-content').find('.thumbnail img').width(220);
+      // Toggle filters when button is clicked (reports filters on "create update" sidebar)
+      $('.view-desk-reports .views-exposed-widget label', context).each(function() {
+        $(this).unbind('click').click(function() {
+          $(this).siblings('.views-widget, .views-operator').slideToggle();
+        });
+      });
 
       // close modal
       $('#close').click(function() {
