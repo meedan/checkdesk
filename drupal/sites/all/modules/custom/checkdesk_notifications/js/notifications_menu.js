6199f2e9d94669b842f54b59586c1442556c36c0
Caio SBA <caiosba@gmail.com>
2013-02-05 22:53:15 -0200

    Ticket #1041: Fixed broken notifications code after refactoring

--- drupal/sites/all/modules/custom/checkdesk_notifications/js/notifications_menu.js
+++ drupal/sites/all/modules/custom/checkdesk_notifications/js/notifications_menu.js
@@ -2,20 +2,12 @@
 
 Drupal.behaviors.meedan_notifications_menu_visibility = {
   attach: function (context, settings) {
-    var block = $('#block-views-my-notifications-block', context);
+    var block = $('#my-notifications', context);
     var title = $('#my-notifications-menu-link', context);
     block.find('.content, h2').hide();
     title.unbind('click');
     title.click(function() {
       var that = $(this);
-      if (block.hasClass('notifications-block-expanded')) {
-        block.find('.content').slideUp('slow', function() {
-          block.removeClass('notifications-block-expanded');
-        });
-      } else {
-        block.addClass('notifications-block-expanded');
-        block.find('.content').slideDown('slow');
-      }
       if (that.find('.notifications-count').html() != '') {
         $.ajax({
           type: 'GET',
@@ -26,20 +18,12 @@ Drupal.behaviors.meedan_notifications_menu_visibility = {
         });
       }
     });
-    $('html').click(function() {
-      block.find('.content').slideUp('slow', function() {
-        block.removeClass('notifications-block-expanded');
-      });
-    });
-    block.click(function(event){
-      event.stopPropagation();
-    });
   }
 };
 
 Drupal.behaviors.meedan_notifications_load_more = {
   attach: function (context, settings) {
-    var block = $('#block-views-my-notifications-block', context);
+    var block = $('#my-notifications', context);
     var container =  block.find('.view-content');
     container.unbind('scroll');
     container.on('scroll', function() {
@@ -52,7 +36,7 @@ Drupal.behaviors.meedan_notifications_load_more = {
 
 Drupal.behaviors.alert_new_notifications = {
   attach: function (context, settings) {
-    var block = $('#block-views-my-notifications-block');
+    var block = $('#my-notifications');
     block.unbind('autorefresh.update');
     block.bind('autorefresh.update', function(e, nid) {
       Drupal.behaviors.meedan_notifications_load_more.attach();
