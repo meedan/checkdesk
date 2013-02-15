0aa322d9c4c3d26c2595c088468a3506e9103c88
James Andres <james.andres@gmail.com>
2013-02-15 17:08:55 -0500

    Improving the error reporting in the add report widget and refactoring Javascript API for usefulness and consistency.

--- drupal/sites/all/modules/custom/checkdesk_bookmarklet/js/checkdesk_bookmarklet_form.js
+++ drupal/sites/all/modules/custom/checkdesk_bookmarklet/js/checkdesk_bookmarklet_form.js
@@ -4,15 +4,33 @@ jQuery(function($) {
 
   // Function to retrive a media preview from a URL
   function getMediaPreview() {
-    var input = $('#edit-field-link-und-0-url');
-    input.addClass('meedan-bookmarklet-loading');
-    $.get(Drupal.settings.basePath + 'checkdesk/media-preview?' + parseInt(Math.random() * 1000000000), { url : input.val() }, function(data) {
-      $('#meedan_bookmarklet_preview_content').html(data);
-      input.removeClass('meedan-bookmarklet-loading');
-      if (data) {
-        $('#meedan_bookmarklet_preview, #edit-body, #edit-graphic-content, #edit-submit').show();
-        // Link was provided, so restore window to its original size
-        window.parent.postMessage('reset', '*');
+    var $input = $('#edit-field-link-und-0-url'),
+        $preview = $('#meedan_bookmarklet_preview_content'),
+        $controls = $('#edit-body, #edit-graphic-content, #edit-submit');
+
+    $input.addClass('meedan-bookmarklet-loading');
+
+    $.ajax({
+      url: Drupal.settings.basePath + 'checkdesk/media-preview?' + parseInt(Math.random() * 1000000000),
+      data: { url : $input.val() },
+      dataType: 'json',
+      success: function (data) {
+        if (data.error) {
+          $preview.addClass('error').html(data.error.message);
+          $controls.hide();
+        } else {
+          window.parent.postMessage('reset', '*');
+          $preview.removeClass('error').html(data.preview);
+          $controls.show();
+        }
+      },
+      error: function (xhr, textStatus, error) {
+        $preview.addClass('error').html(Drupal.t('An error occurred while communicating with Checkdesk. Please try again.'));
+        $controls.hide();
+      },
+      complete: function (xhr, textStatus) {
+        $input.removeClass('meedan-bookmarklet-loading');
+        $('#meedan_bookmarklet_preview').show();
       }
     });
   }
