8d37e1c6d6967da774743ed5f425053fa760abb3
James Andres <james.andres@gmail.com>
2013-03-01 09:17:48 -0500

    Fixing RTL issues with inline errors. Improving theming.

--- drupal/sites/all/themes/checkdesk/assets/js/inline_errors.js
+++ drupal/sites/all/themes/checkdesk/assets/js/inline_errors.js
@@ -1,9 +1,29 @@
 jQuery(function ($) {
 
+// See: http://www.texotela.co.uk/code/jquery/reverse/
+$.fn.reverse = function() {
+  return this.pushStack(this.get().reverse(), arguments);
+};
+
+// Searches all previous elements in the DOM until a match is found
+$.fn.prevALL = function(matchExpr) {
+  // get all the elements in the body, including the body.
+  var $all = $('body').find('*').andSelf();
+
+  // slice the $all object according to which way we're looking
+  $all = $all.slice(0, $all.index(this)).reverse();
+
+  // filter the matches if specified
+  if (matchExpr) $all = $all.filter(matchExpr);
+
+  return $all;
+};
+
 /**
  * Move the messages-inline div right after the form label.
  */
 Drupal.behaviors.checkdeskInlineErrors = {
+
   attach: function (context) {
     $('.messages-inline:not(.cd-inline-errors-processed)', context)
       .addClass('cd-inline-errors-processed')
@@ -11,11 +31,12 @@ Drupal.behaviors.checkdeskInlineErrors = {
         var $msg  = $(this),
             // Risky, relying on the .messages-inline DIV always coming
             // immediately after the form element. This seems to be the case.
-            $item = $msg.prev();
+            $label = $msg.prevALL('label:first');
 
-        $('label', $item).append($msg);
+        $label.after($msg);
       });
   }
+
 };
 
 }(jQuery));
