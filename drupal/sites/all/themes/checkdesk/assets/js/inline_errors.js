e8ce4df1e211e5d6b58eb8561ebd38b00829395f
James Andres <james.andres@gmail.com>
2013-03-01 10:30:27 -0500

    Improving usability of inline errors.

--- drupal/sites/all/themes/checkdesk/assets/js/inline_errors.js
+++ drupal/sites/all/themes/checkdesk/assets/js/inline_errors.js
@@ -1,42 +1,60 @@
 jQuery(function ($) {
+  "use strict";
 
-// See: http://www.texotela.co.uk/code/jquery/reverse/
-$.fn.reverse = function() {
-  return this.pushStack(this.get().reverse(), arguments);
-};
+  // See: http://www.texotela.co.uk/code/jquery/reverse/
+  $.fn.reverse = function() {
+    return this.pushStack(this.get().reverse(), arguments);
+  };
 
-// Searches all previous elements in the DOM until a match is found
-$.fn.prevALL = function(matchExpr) {
-  // get all the elements in the body, including the body.
-  var $all = $('body').find('*').andSelf();
+  // Searches all previous elements in the DOM until a match is found
+  $.fn.prevALL = function(matchExpr) {
+    // get all the elements in the body, including the body.
+    var $all = $('body').find('*').andSelf();
 
-  // slice the $all object according to which way we're looking
-  $all = $all.slice(0, $all.index(this)).reverse();
+    // slice the $all object according to which way we're looking
+    $all = $all.slice(0, $all.index(this)).reverse();
 
-  // filter the matches if specified
-  if (matchExpr) $all = $all.filter(matchExpr);
+    // filter the matches if specified
+    if (matchExpr) {
+      $all = $all.filter(matchExpr);
+    }
 
-  return $all;
-};
+    return $all;
+  };
 
-/**
- * Move the messages-inline div right after the form label.
- */
-Drupal.behaviors.checkdeskInlineErrors = {
+  /**
+   * Move the messages-inline div right after the form label.
+   */
+  Drupal.behaviors.checkdeskInlineErrors = {
 
-  attach: function (context) {
-    $('.messages-inline:not(.cd-inline-errors-processed)', context)
-      .addClass('cd-inline-errors-processed')
-      .each(function () {
-        var $msg  = $(this),
-            // Risky, relying on the .messages-inline DIV always coming
-            // immediately after the form element. This seems to be the case.
-            $label = $msg.prevALL('label:first');
+    attach: function (context) {
+      var $firstError;
 
-        $label.after($msg);
-      });
-  }
+      $('.messages-inline:not(.cd-inline-errors-processed)', context)
+        .addClass('cd-inline-errors-processed')
+        .each(function () {
+          var $msg  = $(this),
+              // Risky, relying on the .messages-inline DIV always coming
+              // immediately after the form element. This seems to be the case.
+              $label = $msg.prevALL('label:first');
 
-};
+          if (!$firstError) {
+            $firstError = $msg;
+          }
+
+          $label.after($msg);
+        });
+
+      // Scroll the browser to the fist displayed error
+      if ($firstError) {
+        setTimeout(function () {
+          $('html, body').animate({
+            scrollTop: $firstError.offset().top - $firstError.outerHeight()
+          }, 'slow');
+        }, 2000);
+      }
+    }
+
+  };
 
 }(jQuery));
