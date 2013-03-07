3aa820780011e3bd9d6fa94378172a61d361a1bc
Caio SBA <caiosba@gmail.com>
2013-03-07 01:24:55 -0300

    Ticket #1134: Fixing submit report on IE 8

--- drupal/sites/all/modules/custom/checkdesk_bookmarklet/js/checkdesk_bookmarklet.js
+++ drupal/sites/all/modules/custom/checkdesk_bookmarklet/js/checkdesk_bookmarklet.js
@@ -1,46 +1,50 @@
 (function() {
 
   // Communication between inner window and outer window happens using messages
-  window.addEventListener("message",
-    function(e) {
-      switch(e.data) {
+  var messageCallback = function(e) {
+    switch(e.data) {
 
-        // Close iframe
-        case 'close':
-          jQuery('#meedan_bookmarklet_cont, #meedan_bookmarklet_mask').fadeOut(500);
-          jQuery('#menu-submit-report').removeClass('open');
-          break;
-        
-        // Scale iframe after report was submitted or before link is provided
-        case 'submit':
-        case 'nolink':
-          jQuery('#meedan_bookmarklet_cont').addClass('meedan-bookmarklet-collapsed');
-          break;
-
-        // Send another report
-        case 'reset':
-          jQuery('#meedan_bookmarklet_cont').removeClass('meedan-bookmarklet-collapsed');
-          break;
-
-        // Bookmarklet loaded
-        case 'loaded':
-          // Adjust bookmarklet modal position for internal bookmarklet
-          if (jQuery('#menu-submit-report').length > 0) {
-            var offset = jQuery('#menu-submit-report').offset();
-            jQuery('#meedan_bookmarklet_cont').css('top', (parseInt(offset.top) + 26) + 'px');
-          }
-          jQuery('#meedan_bookmarklet_cont').show();
-          break;
-
-        // Destroy bookmarklet
-        case 'destroy':
-          jQuery('#meedan_bookmarklet_cont, #meedan_bookmarklet_mask').remove();
-          jQuery('#menu-submit-report').removeClass('open');
-          break;
+      // Close iframe
+      case 'close':
+        jQuery('#meedan_bookmarklet_cont, #meedan_bookmarklet_mask').fadeOut(500);
+        jQuery('#menu-submit-report').removeClass('open');
+        break;
       
-      }
-    },
-    false
-  );
+      // Scale iframe after report was submitted or before link is provided
+      case 'submit':
+      case 'nolink':
+        jQuery('#meedan_bookmarklet_cont').addClass('meedan-bookmarklet-collapsed');
+        break;
+
+      // Send another report
+      case 'reset':
+        jQuery('#meedan_bookmarklet_cont').removeClass('meedan-bookmarklet-collapsed');
+        break;
+
+      // Bookmarklet loaded
+      case 'loaded':
+        // Adjust bookmarklet modal position for internal bookmarklet
+        if (jQuery('#menu-submit-report').length > 0) {
+          var offset = jQuery('#menu-submit-report').offset();
+          jQuery('#meedan_bookmarklet_cont').css('top', (parseInt(offset.top) + 26) + 'px');
+        }
+        jQuery('#meedan_bookmarklet_cont').show();
+        break;
+
+      // Destroy bookmarklet
+      case 'destroy':
+        jQuery('#meedan_bookmarklet_cont, #meedan_bookmarklet_mask').remove();
+        jQuery('#menu-submit-report').removeClass('open');
+        break;
+    
+    }
+  };
+
+  if (!window.addEventListener) {
+    window.attachEvent('onmessage', messageCallback);
+  }
+  else {
+    window.addEventListener('message', messageCallback, false);
+  }
 
 })();
