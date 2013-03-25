32104088fd85ee7b387296c7bd842039c0be6076
James Andres <james.andres@gmail.com>
2013-03-25 11:21:21 -0400

    Revert "Correcting problems with usage of Drupal.behaviors in report.js."
    This reverts commit bb5a1708e5093ee8a8a203024b3eb74dca294ffc.

--- drupal/sites/all/themes/checkdesk/assets/js/report.js
+++ drupal/sites/all/themes/checkdesk/assets/js/report.js
@@ -3,9 +3,9 @@
   'use strict';
 
 	Drupal.behaviors.reports = {
-		attach: function (context) {
+		attach: function (context, settings) {
 		  // Show report activity
-      $('.report-activity > header', context).unbind('click').click(function(event) {
+      $('.report-activity > header').unbind('click').click(function(event) {
         var target = $(this),
             element = target.parent();
         if (element.find('.activity-wrapper').is(':visible')) {
@@ -20,18 +20,18 @@
       });
 
       // Remove duplicates added incrementally by views_autorefresh after loading more content with views_load_more
-      $('.view-liveblog', context).unbind('views_load_more.new_content').bind('views_load_more.new_content', function(event, content) {
+      $('.view-liveblog').unbind('views_load_more.new_content').bind('views_load_more.new_content', function(event, content) {
         $(content).find('section.node-post').each(function() {
           $('.view-liveblog #' + $(this).attr('id')).eq(0).parents('.views-row').remove();
         });
       });
-      $('.view-desk-reports', context).unbind('views_load_more.new_content').bind('views_load_more.new_content', function(event, content) {
+      $('.view-desk-reports').unbind('views_load_more.new_content').bind('views_load_more.new_content', function(event, content) {
         $(content).find('.report-row-container').each(function() {
           $('.view-desk-reports #' + $(this).attr('id')).eq(0).parents('.views-row').remove();
         });
       });
 
-			$('a.twitter', context).click(function(event) {
+			$('a.twitter').click(function(event) {
 				event.preventDefault();
 				// set URL
 				var loc = $(this).attr('href'),
@@ -41,7 +41,7 @@
 				openShareWindow('twitter', loc, title);
 			});
 
-			$('a.facebook', context).click(function(event) {
+			$('a.facebook').click(function(event) {
 				event.preventDefault();
 				// set URL
 				var loc = $(this).attr('href'),
@@ -51,7 +51,7 @@
 				openShareWindow('facebook', loc, title);
 			});
 
-			$('a.google', context).click(function(event) {
+			$('a.google').click(function(event) {
 				event.preventDefault();
 				// set URL
 				var loc = $(this).attr('href'),
@@ -79,8 +79,8 @@
 
 	// filters for reports inside sidebar
 	Drupal.behaviors.reportFilters = {
-		attach: function (context) {
-			$('.panel-toggle', context).unbind('click').click(function(event) {
+		attach: function (context, settings) {
+			$('.panel-toggle').unbind('click').click(function(event) {
 				var target = $(this),
 				    element = target.parent().attr('id');
 				if ($('#'+ element + ' .panel-content').is(':visible')) {
@@ -92,8 +92,6 @@
 				}
 
 			});
-      // FIXME: Using $(document) here will not work when behavior is
-      //        re-attached. Need to make use of context or use a static variable.
 			// hide when clicked outside
 			$(document).mouseup(function(event){
 				var container = $('.panel-content');
@@ -105,21 +103,17 @@
 			var bodyHeight = $(window).height(),
 				  buffer = 160;
 			bodyHeight = bodyHeight - buffer;
-			$('#sidebar-first.column .view-desk-reports', context).height(bodyHeight);
+			$('#sidebar-first.column .view-desk-reports').height(bodyHeight);
 
-      // FIXME: Using $(window) here will not work when behavior is
-      //        re-attached. Need to make use of context or use a static variable.
 			$(window).resize(function(){
 				var bodyHeight = $(window).height();
 				bodyHeight = bodyHeight - buffer;
 				// console.log(bodyHeight);
-				$('#sidebar-first.column .view-desk-reports', context).height(bodyHeight);
+				$('#sidebar-first.column .view-desk-reports').height(bodyHeight);
 			});
 
-      // FIXME: Using $(window) here will not work when behavior is
-      //        re-attached. Need to make use of context or use a static variable.
 			// close panel
-			$('#close', context).click(function(event) {
+			$('#close').click(function(event) {
 				$('.panel-content').hide();
 			});
 
