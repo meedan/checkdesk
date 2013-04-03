f9d41c1eea3e90140240dbc14e7dfab5d26925fe
James Andres <james.andres@gmail.com>
2013-04-03 13:58:13 -0400

    Refs #1210: Synchronizing all embedded reports HTML to match the new reports layout from Maya.

--- drupal/sites/all/modules/meedan/meedan_oembed/theme/oembed-facebook.tpl.php
+++ drupal/sites/all/modules/meedan/meedan_oembed/theme/oembed-facebook.tpl.php
@@ -1,24 +1,37 @@
-<?php if ($author_img) : ?>
-  <div class="author_img"><?php print $author_img ?></div>
-<?php endif ?>
-<?php if ($author_name) : ?>
-  <div class="author_name"><?php print $author_name ?></div>
-<?php endif ?>
-<?php if ($content) : ?>
-  <div class="content"><?php print $content ?></div>
-<?php endif ?>
-<?php if ($fb_picture) : ?>
-  <div class="fb-picture"><?php print $fb_picture ?></div>
-<?php endif ?>
-<?php if ($fb_link_title) : ?>
-  <div class="fb-link-title"><?php print $fb_link_title ?></div>
-<?php endif ?>
-<?php if ($fb_link_desc) : ?>
-  <div class="fb-link-desc"><?php print $fb_link_desc ?></div>
-<?php endif ?>
-<?php if ($favicon_link) : ?>
-  <div class="favicon"><?php print $favicon_link ?></div>
-<?php endif ?>
-<?php if ($domain_link) : ?>
-  <div class="domain"><?php print $domain_link ?></div>
-<?php endif ?>
+<?php
+/**
+ * @file
+ * Default template file for Facebook oembed data
+ */
+?>
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <a href="<?php print $original_url; ?>"<?php print $title_attributes; ?>><?php print render($title); ?></a>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <div<?php print $content_attributes; ?>>
+    <?php print render($content); ?>
+  </div>
+
+  <?php if ($fb_picture) : ?>
+    <div class="fb-picture"><?php print $fb_picture ?></div>
+  <?php endif ?>
+  <?php if ($fb_link_title) : ?>
+    <div class="fb-link-title"><?php print $fb_link_title ?></div>
+  <?php endif ?>
+  <?php if ($fb_link_desc) : ?>
+    <div class="fb-link-desc"><?php print $fb_link_desc ?></div>
+  <?php endif ?>
+
+  <?php if ($favicon_link) : ?>
+    <div class="favicon"><?php print $favicon_link ?></div>
+  <?php endif ?>
+  <?php if ($embed->author_name) : ?>
+    <div class="author"><?php print $embed->author_url ? l($embed->author_name, $embed->author_url) : $embed->author_name; ?></div>
+  <?php endif ?>
+  <?php if (isset($embed_error)) : ?>
+    <div class="embederror"><?php print $embed_error ?></div>
+  <?php endif ?>
+</div>
