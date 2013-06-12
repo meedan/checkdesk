23ab20fbfde82fd1d6fd98aa6eff8a4247a8dc1b
designbysoil <designbysoil@gmail.com>
2013-06-12 02:09:53 +0000

    Ticket #1405: Beginning to retheme story and updates.

--- drupal/sites/all/themes/checkdesk/templates/node--post.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/node--post.tpl.php
@@ -1,16 +1,12 @@
 <section id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <article class="update">
-    <p class="update-by">
-    	<?php if (isset($user_avatar)) : ?>
-            <?php print $user_avatar; ?>
-        <?php endif; ?>
-    	<?php print $update_creation_info; ?>
-    </p>
-    <div class="update-body">
-      <?php print render($content['body']); ?>
-    </div>
-    <p><?php print render($content['field_desk']); ?></p>
     <div class="update-footer">
+      <div class="update-by">
+        <?php if (isset($user_avatar)) : ?>
+              <?php print $user_avatar; ?>
+          <?php endif; ?>
+        <?php print $update_creation_info; ?>
+      </div>
       <div class="update-actions">
         <?php print render($content['links']); ?>
       </div>
@@ -18,5 +14,10 @@
         <?php print render($content['custom_comments']); ?>
       </div>
     </div>
+    
+    <div class="update-body">
+      <?php print render($content['body']); ?>
+    </div>
+    
   </article>
 </section>
