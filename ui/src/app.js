30ca4f1f3023108d8803043db7974104ed4bb783
James Andres <james.andres@gmail.com>
2013-06-18 08:18:00 -0400

    UI: Adding docular support for inline code documentation.

--- ui/src/app.js
+++ ui/src/app.js
@@ -1,3 +1,12 @@
+/**
+ * @doc app
+ * @id index
+ * @name Checkdesk App
+ *
+ * @description
+ * The Checkdesk application is an AngularJS based front-end consuming
+ * web-servicesprovided by a Drupal powered back-end.
+ */
 var app = angular.module('Checkdesk', [
       'pascalprecht.translate',
       'cd.l10n',
--- ui/src/modules/cd.l10n/cd.l10n.js
+++ ui/src/modules/cd.l10n/cd.l10n.js
@@ -1,7 +1,25 @@
+/**
+ * @ngdoc module
+ * @name l10n
+ *
+ * @description
+ * The `cd.l10n` module manages all translation and localization aspects of
+ * the Checkdesk app.
+ */
 angular.module('cd.l10n', ['pascalprecht.translate', 'cd.translationUI'])
+
+  /**
+   * @ngdoc function
+   * @name l10n.class:config
+   * @requires $translateProvider
+   *
+   * @description
+   * Configures all languages necessary for the Checkdesk app and sets the
+   * cdTranslationUI service as the missing translation handler for $translate.
+   */
   .config(['$translateProvider', function ($translateProvider) {
     // Initially empty translation tables
-    $translateProvider.translations('en', {});
+    $translateProvider.translations('en-NG', {});
     $translateProvider.translations('ar', {});
 
     $translateProvider.uses('ar');
@@ -11,10 +29,23 @@ angular.module('cd.l10n', ['pascalprecht.translate', 'cd.translationUI'])
 
     $translateProvider.useMissingTranslationHandler('cdTranslationUI');
   }])
+
+  /**
+   * @ngdoc function
+   * @name l10n.class:run
+   * @requires $translate
+   * @requires cdTranslationUI
+   * @requires Translation
+   *
+   * @description
+   * On run, uses the Translation service to fetch the necessary translation
+   * strings from the Drupal API. Once retrieved the strings are installed
+   * in the $translate service and the interface is refreshed.
+   */
   .run(['$translate', 'cdTranslationUI', 'Translation', function ($translate, cdTranslationUI, Translation) {
     Translation.query({}, function (translations) {
       var translationTable,
-          languages = ['ar', 'en'],
+          languages = ['ar', 'en-NG'],
           language, translation, i, j;
 
       // Get a reference to the translation table
