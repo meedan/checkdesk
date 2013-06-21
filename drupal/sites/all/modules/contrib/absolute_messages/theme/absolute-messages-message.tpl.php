d01ac1054e6721ac34d70fe7ce879f4245a248da
infojunkie <karim.ratib@gmail.com>
2013-06-21 13:38:06 -0700

    Hide update title field #1454

--- drupal/sites/all/modules/contrib/absolute_messages/theme/absolute-messages-message.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="absolute-messages-message absolute-messages-<?php print $vars['type']; ?>">
-  <div class="absolute-messages-actions">
-    <a class="absolute-messages-dismiss" title="<?php print $vars['dismiss']; ?>">x</a>
-  </div>
-  <div class="content"><?php print $vars['message']; ?></div>
-</div>
