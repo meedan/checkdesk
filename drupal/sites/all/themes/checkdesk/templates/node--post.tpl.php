4222a04b82a4d1e1d33c5eecc2453c19c46b5725
designbysoil <designbysoil@gmail.com>
2013-06-23 20:10:07 +0000

    Ticket #1462: Adding edit link to updates.

--- drupal/sites/all/themes/checkdesk/templates/node--post.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/node--post.tpl.php
@@ -3,7 +3,6 @@
   // dsm($node);
 ?>
 <section id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <a name="update-<?php print $node->nid; ?>" class="scroll"></a>
   <article class="update">
     <div class="update-body">
       <?php print render($content['body']); ?>
@@ -11,17 +10,22 @@
     <div class="update-footer">
       <ul class="update-meta">
         <li class="update-at">
-          <span class="icon-time"></span> <?php print $created_at; ?>
+          <a name="update-<?php print $node->nid; ?>"><span class="icon-time"></span> <?php print $created_at; ?></a>
         </li>
         <li class="update-by">
-          <?php if (isset($user_avatar)) : ?>
+          <?php if (isset($user_avatar)) { ?>
             <?php print $user_avatar; ?>
-          <?php endif; ?>
+          <?php } ?>
           <?php print $created_by; ?>
         </li>
         <!-- <li class="share">
           <a href="#"><span class="icon-share"></span> <?php print t('Share'); ?></a>
         </li> -->
+        <?php if (user_access('administer nodes')) { ?>
+          <li class="update-edit">
+            <?php print l('<span class="icon-edit-sign"></span>'. t('Edit'), 'node/' . $node->nid . '/edit', array('html'=>TRUE)); ?>
+          </li>
+        <?php } ?>
       </ul>
     </div>
   </article>
