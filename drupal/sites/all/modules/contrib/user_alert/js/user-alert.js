4ece429f49ca89cd9c231309e0c97d61d0916e04
Caio SBA <caiosba@gmail.com>
2013-07-15 23:45:35 -0300

    Ticket #1480: Do not use cookies on user_alert

--- drupal/sites/all/modules/contrib/user_alert/js/user-alert.js
+++ drupal/sites/all/modules/contrib/user_alert/js/user-alert.js
@@ -1,16 +1,6 @@
 (function ($) {
   Drupal.behaviors.user_alert_get_message = {
     attach: function(context) {
-      /*
-      $.ajax({
-        type: 'GET',
-        url: Drupal.settings.basePath + Drupal.settings.pathPrefix + Drupal.settings.user_alert.url_prefix + 'js/user-alert/get-message',
-        success: function(response) {
-          $('.block-user-alert').html(response[1].data);
-        }
-      });
-      */
-    	
       $('body').delegate('div.user-alert-close a', 'click', function() {
         id = $(this).attr('rel');
         $.ajax({
