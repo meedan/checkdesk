ea0ca787c317b267db4832f50c2dd6d0494cbee5
James Andres <james.andres@gmail.com>
2013-07-16 16:31:46 +0100

    Refs #1026: Adding textarea to share embed code for reports.

--- drupal/sites/all/themes/checkdesk/template.php
+++ drupal/sites/all/themes/checkdesk/template.php
@@ -742,6 +742,9 @@ function checkdesk_links__node($variables) {
       if (isset($links['checkdesk-share-google'])) {
         $output .= '<li>' . l($links['checkdesk-share-google']['title'], $links['checkdesk-share-google']['href'], $links['checkdesk-share-google']) . '</li>';
       }
+      if (isset($links['checkdesk-share-embed'])) {
+        $output .= '<li>' . l($links['checkdesk-share-embed']['title'], $links['checkdesk-share-google']['href'], $links['checkdesk-share-embed']) . '</li>';
+      }
       $output .= '</ul></li>'; 
     }
 
