76a7137f16d6b3d99b68d008f18355cf2decc6d3
James Andres <james.andres@gmail.com>
2013-07-23 11:23:42 +0100

    Refs #1026, #1511: Adding footer to embed and theming. Ensuring links within the embed open in a new window.

--- drupal/sites/all/themes/checkdesk_embed/template.php
+++ drupal/sites/all/themes/checkdesk_embed/template.php
@@ -9,14 +9,6 @@ function checkdesk_embed_preprocess_page(&$variables) {
 
   // Never display user alerts in the embed
   unset($variables['page']['content']['user_alert_user_alert']);
-}
 
-/**
- * Override or insert variables into the region template.
- */
-function checkdesk_embed_preprocess_region(&$variables) {
-  // Invoke parent preprocessor
-  checkdesk_preprocess_region($variables);
-
-  // TODO: Update the footer region
+  $variables['content_url'] = url('node/' . arg(1), array('absolute' => TRUE));
 }
