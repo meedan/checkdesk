c5717ed1d1b6b6087a159d5e2bbb9f163509d330
James Andres <james.andres@gmail.com>
2013-07-23 09:53:43 +0100

    Refs #1026, #1511: Hiding user alerts inside embeds.

--- drupal/sites/all/themes/checkdesk_embed/template.php
+++ drupal/sites/all/themes/checkdesk_embed/template.php
@@ -3,6 +3,17 @@
 /**
  * Override or insert variables into the region template.
  */
+function checkdesk_embed_preprocess_page(&$variables) {
+  // Invoke parent preprocessor
+  checkdesk_preprocess_page($variables);
+
+  // Never display user alerts in the embed
+  unset($variables['page']['content']['user_alert_user_alert']);
+}
+
+/**
+ * Override or insert variables into the region template.
+ */
 function checkdesk_embed_preprocess_region(&$variables) {
   // Invoke parent preprocessor
   checkdesk_preprocess_region($variables);
