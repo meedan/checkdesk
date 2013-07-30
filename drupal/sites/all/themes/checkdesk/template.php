dbb956633ac7d19a2d6c127ec3e84665300aac03
Caio SBA <caiosba@gmail.com>
2013-07-30 23:50:00 +0300

    Ticket #1575: Fixing quotes on page titles

--- drupal/sites/all/themes/checkdesk/template.php
+++ drupal/sites/all/themes/checkdesk/template.php
@@ -106,7 +106,7 @@ function checkdesk_preprocess_html(&$variables) {
   $head_title = array();
   $title = drupal_get_title();
   if (!empty($title)) {
-    $head_title[] = $title;
+    $head_title[] = htmlspecialchars_decode($title);
   }
   $head_title[] = variable_get('site_name', 'Drupal');
   $variables['head_title'] = strip_tags(implode(' | ', $head_title));
