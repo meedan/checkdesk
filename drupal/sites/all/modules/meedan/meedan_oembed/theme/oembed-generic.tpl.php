d763e2e3dc5634c395829d6919ae0da5e287aa91
designbysoil <designbysoil@gmail.com>
2013-08-27 20:37:45 +0000

    Adding original page title and URL for generic oembeds.

--- drupal/sites/all/modules/meedan/meedan_oembed/theme/oembed-generic.tpl.php
+++ drupal/sites/all/modules/meedan/meedan_oembed/theme/oembed-generic.tpl.php
@@ -1,13 +1,18 @@
 <?php
 /**
  * @file
- * Default template file for generic oembed data
+ * Custom template file for oembed data
+ * This is the template that is used for generic embeds
  */
+  if(isset($embed->provider_name)) {
+    $provider = strtolower($embed->provider_name);
+    $provider_class_name = str_replace('.', '_', $provider);
+  }
 ?>
-<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<div class="<?php print $classes . ' ' . $provider_class_name; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
-  <?php if (isset($title)): ?>
-    <a href="<?php print $original_url; ?>"<?php print $title_attributes; ?>><?php print render($title); ?></a>
+  <?php if ($title): ?>
+    <h3 class="title"><a href="<?php print $embed->original_url; ?>"<?php print $title_attributes; ?>><?php print render($title); ?></a></h3>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
@@ -15,12 +20,22 @@
     <?php print render($content); ?>
   </div>
 
-  <?php if (isset($favicon_link)) : ?>
-    <div class="favicon"><?php print $favicon_link ?></div>
-  <?php endif ?>
-  <?php if (isset($domain_link)) : ?>
-    <div class="domain"><?php print $domain_link ?></div>
-  <?php endif ?>
+  <div class="oembed-attributes">
+    <?php if ($favicon_link) : ?>
+      <div class="favicon"><?php print $favicon_link ?></div>
+    <?php endif ?>
+    <?php if (isset($embed->author_name)) : ?>
+      <div class="author"><?php print $embed->author_url ? l($embed->author_name, $embed->author_url) : $embed->author_name; ?></div>
+    <?php elseif (isset($domain_link)) : ?>
+      <div class="domain"><?php print $domain_link ?></div>
+    <?php endif ?>
+
+    <?php if (isset($embed->original_url)) : ?>
+      <div class="permalink">
+        <?php print l('<span class="icon-link"></span>', $embed->original_url, array('attributes' => array('title' => t('View original')), 'html' => TRUE)); ?>
+      </div>
+    <?php endif ?>
+  </div>
   <?php if (isset($embed_error)) : ?>
     <div class="embederror"><?php print $embed_error ?></div>
   <?php endif ?>
