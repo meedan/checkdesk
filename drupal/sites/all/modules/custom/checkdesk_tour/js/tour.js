895a474f83ccd796ccc3fb21b8c0db1152c1c3fb
Caio SBA <caiosba@gmail.com>
2013-12-26 08:13:00 +0300

    Ticket #1887: Do not hide popover on mouse over

--- drupal/sites/all/modules/custom/checkdesk_tour/js/tour.js
+++ drupal/sites/all/modules/custom/checkdesk_tour/js/tour.js
@@ -67,6 +67,9 @@ jQuery(function () {
             // If we set margin-left directly on CSS, it won't work on Firefox
             // FIXME: Argh, hard-coded
             jQuery('.tour-step-background').css('margin-left', '-200px');
+            
+            // Do not hide popovers on mouse over
+            jQuery182('#' + this.id).off('mouseenter').off('mouseleave');
           }
         });
 
