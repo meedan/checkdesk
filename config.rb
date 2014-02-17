994c40218f5bb3171e883316d65583490141260c
Chris Blow <cgblow@gmail.com>
2014-02-17 13:19:44 -1000

    refactor using new structure

--- config.rb
+++ config.rb
@@ -1,5 +1,4 @@
 require "compass_twitter_bootstrap"
-require "susy"
 
 line_comments = false
 
@@ -7,11 +6,10 @@ drupal_dir = "drupal"
 themes_dir = "sites/all/themes/"
 assets_dir = "checkdesk/assets"
 full_assets_dir = File.join(drupal_dir, themes_dir, assets_dir)
-http_path       = "/"
-css_dir         = File.join(full_assets_dir, "css")
-sass_dir        = File.join(full_assets_dir, "scss")
-bower_dir = File.join(full_assets_dir, "bower_components")
-images_dir      = File.join(full_assets_dir, "imgs")
+http_path = "/"
+css_dir = File.join(full_assets_dir, "css")
+sass_dir = File.join(full_assets_dir, "scss")
+images_dir = File.join(full_assets_dir, "imgs")
 javascripts_dir = File.join(full_assets_dir, "js")
 
 # icon webfonts from bowerbird
@@ -19,5 +17,8 @@ fonts_dir = File.join(full_assets_dir, "fonts")
 http_fonts_path = File.join(http_path, themes_dir, assets_dir, "fonts")
 relative_assets = false
 
+# Add bower components to our path
+add_import_path "drupal/sites/all/themes/checkdesk/assets/bower_components/susy/sass"
+add_import_path "drupal/sites/all/themes/checkdesk/assets/bower_components/modular-scale/stylesheets"
+
 output_style = (environment == :production) ? :compressed : :expanded
-# sass_options = (environment == :production) ? { :debug_info => false } : { :debug_info => true } 
