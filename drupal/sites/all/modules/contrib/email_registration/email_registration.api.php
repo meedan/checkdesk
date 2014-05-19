241618a69fd6e32bdb88bda4b9db18a018efc606
Mohammed El-Sawy <melsawy.be7eri@gmail.com>
2014-05-19 09:16:55 +0200

    Issue #1926 update devel, email_registration and entity

--- drupal/sites/all/modules/contrib/email_registration/email_registration.api.php
+++ drupal/sites/all/modules/contrib/email_registration/email_registration.api.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Documentation for email_registration API.
+ * Documentation for email_registration module API.
  */
 
 /**
@@ -17,16 +17,19 @@
  * to generate a username (return a string to be used as the username, NULL
  * to have email_registration generate it).
  *
- * @param $edit
+ * @param array $edit
  *   The array of form values submitted by the user.
- * @param $account
+ * @param object $account
  *   The user object on which the operation is being performed.
  *
- * @return
+ * @return string
  *   A string defining a generated username.
  */
 function hook_email_registration_name($edit, $account) {
-  return 'u' . $account->uid;
+  // Your hook implementation should ensure that the resulting string
+  // works as a username. You can use email_registration_cleanup_username($name)
+  // to clean up the name.
+  return email_registration_cleanup_username('u' . $account->uid);
 }
 
 /**
