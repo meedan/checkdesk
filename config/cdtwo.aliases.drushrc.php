1a366fe47d24d9a6498987b6cc9a9eced04fecb5
Benjamin Foote <github@bnf.net>
2014-07-03 14:13:27 -0700

    cdtwo drush aliases

---
+++ config/cdtwo.aliases.drushrc.php
@@ -0,0 +1,44 @@
+<?php
+
+$aliases['dev'] = array(
+  'site-list' => array('@dev.meedan'),
+);
+
+// dev aliases
+$aliases['dev-alias'] = array (
+  'remote-host' => 'cdtwo.dev.meedan.net',
+  'ssh-options' => '-p 8922',
+  'path-aliases' => array (
+    '%drush' => '/usr/share/php/drush',
+  ),
+);
+$aliases['dev.meedan'] = array (
+  'parent' => '@dev-alias',
+  'uri' => 'cdtwo.dev.meedan.net',
+  'root' => '/var/www/cdtwo/current/drupal',
+);
+
+// dev template
+$aliases['local-alias'] = array(
+  'target-command-specific' => array(
+    'sql-sync' => array(
+      'sanitize' => TRUE,
+      'confirm-sanitizations' => TRUE,
+      'no-ordered-dump' => TRUE,
+      'no-cache' => TRUE,
+      'create-db' => TRUE,
+      'enable' => array(
+        'checkdesk_devel_feature',
+      ),
+    ),
+  ),
+);
+$aliases['local'] = array(
+  'parent' => '@local-alias',
+  'uri' => 'checkdesk.local',
+  'root' => '/var/www/checkdesk/drupal',
+);
+
+include "cdtwo.aliases.local.php"
+
+?>
