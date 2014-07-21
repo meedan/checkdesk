be6edce4229d9a7763a4b361b6ba49c25d03c965
Mohammed El-Sawy <melsawy.be7eri@gmail.com>
2014-07-21 13:05:54 +0200

    Ticket #2983 - render update

--- drupal/sites/all/themes/checkdesk/templates/node--post--checkdesk-collaborate.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/node--post--checkdesk-collaborate.tpl.php
@@ -11,8 +11,16 @@
     <div class="checkdesk-update-reports">
      <?php print $update_reports; ?>
     </div>
-    <div class="checkdesk-update-reports-count">
-     <?php print $update_reports_count; ?>
+    <div class="collaborate-update-footer">
+        <ul class="collaborate-update">
+           <li class="update-reports-count"><?php print $update_reports_count; ?></li>
+           <li class="update-share"><?php print render($content['links']); ?></li>
+           <?php if (in_array('administrator', $user->roles) || in_array('journalist', $user->roles)) { ?>
+           <li class="update-edit">
+            <?php print l('<span class="icon-pencil-square"></span>'. t('Edit'), 'node/' . $node->nid . '/edit', array('query' => drupal_get_destination(), 'html'=>TRUE)); ?>
+           </li>
+           <?php } ?>
+        </ul>
     </div>
     <?php } ?>
   </article>
