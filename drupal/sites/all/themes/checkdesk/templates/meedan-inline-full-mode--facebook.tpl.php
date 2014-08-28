cee621f4769789fe36d687e0aa8f402f01938a0d
designbysoil <designbysoil@gmail.com>
2014-08-28 22:43:13 +0000

    Adding templates for Facebook embeds and generic inline thumbnail formatter.

--- drupal/sites/all/themes/checkdesk/templates/meedan-inline-full-mode--facebook.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/meedan-inline-full-mode--facebook.tpl.php
@@ -3,9 +3,15 @@
     <div class="inline-attachment-bar"><div class="indent"></div></div>
     <div class="media-holder media-inline-holder">
       <div class="media">
-        <div class="inline-holder inline-facebook-holder facebook-holder">
-          <?php print $embed->html; ?>
-        </div>
+        <?php if(isset($embed->html)) : ?>
+          <div class="inline-holder inline-facebook-holder facebook-holder">
+            <?php print $embed->html; ?>
+          </div>
+        <?php elseif (isset($full_image)) : ?>
+          <div class="img-holder">
+            <?php print $full_image; ?>
+          </div>
+        <?php endif ;?>
       </div>
       <?php if(isset($report_status['status'])) : ?>
         <span class="inline-attachment-status media-status">
