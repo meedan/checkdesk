4df89e9d67f3afe3abc96f42ae64aac693a04620
Mohammed El-Sawy <melsawy.be7eri@gmail.com>
2014-09-11 12:39:34 +0300

    Issue #3157

--- drupal/sites/all/modules/custom/checkdesk_notifications/ping.php
+++ drupal/sites/all/modules/custom/checkdesk_notifications/ping.php
@@ -1,9 +1,9 @@
 <?php
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past (HTTP 1.0)
-//$server_name = $_SERVER['HTTP_HOST'];
-$server_name = 'default';
-  require("../../../../{$server_name}/settings.php");
+$server_name = $_SERVER['HTTP_HOST'];
+
+require("../../../../{$server_name}/settings.php");
 
 // Connect to the database
 global $databases;
