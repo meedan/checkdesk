654045365a5c58e9d015c1815f4f104a0a2ac8fb
designbysoil <designbysoil@gmail.com>
2014-09-11 22:28:18 +0000

    Adding image caption for lead image via imagefieldcaption module.

---
+++ drupal/sites/all/modules/contrib/image_field_caption/image_field_caption.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * $image - contains the image html rendered by Drupal
+ * $caption - contains the image field caption string
+ */
+?>
+<?php print $image; ?>
+<blockquote class="image-field-caption">
+  <?php print $caption; ?>
+</blockquote>
