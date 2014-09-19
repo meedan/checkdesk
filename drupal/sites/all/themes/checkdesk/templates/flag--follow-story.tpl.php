13c918def7643ca4e9ee644a8254dac8d19ffe45
designbysoil <designbysoil@gmail.com>
2014-09-19 19:49:43 +0000

    Ticket #3174: Follow button with count.

--- drupal/sites/all/themes/checkdesk/templates/flag--follow-story.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/flag--follow-story.tpl.php
@@ -53,10 +53,10 @@ $follow_count = $flag_follow->get_count($entity_id);
 <?php endif; ?>
 <span class="<?php print $flag_wrapper_classes; ?>">
   <?php if ($link_href): ?>
-    <?php if($follow_count) :?>
-      <span class="follow_count"> <?php print $follow_count; ?></span>
-    <?php endif; ?>
     <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow"><?php print $link_text; ?></a><span class="flag-throbber">&nbsp;</span>
+    <?php if($follow_count) :?>
+      <span class="follow-count"> <?php print $follow_count; ?></span>
+    <?php endif; ?>
   <?php else: ?>
     <span class="<?php print $flag_classes ?>"><?php print $link_text; ?></span>
   <?php endif; ?>
