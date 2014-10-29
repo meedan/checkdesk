80c186f6ff292f02317e30e5b3708cdf21dd2654
Mohammed El-Sawy <melsawy.be7eri@gmail.com>
2014-10-29 15:55:34 +0200

    Ticket #3100 - update form

---
+++ drupal/sites/all/modules/custom/checkdesk_core/js/checkdesk_active_stories.js
@@ -0,0 +1,25 @@
+
+jQuery(function() {
+    jQuery("#edit-cd-field-old-stories").chosen();
+
+    if (jQuery('#edit-cd-field-old-stories-check').is(':checked')) {
+        jQuery('#edit-cd-field-old-stories').attr('disabled', false).trigger("chosen:updated");
+        jQuery('#edit-field-desk-und').attr('disabled', true).trigger("chosen:updated");
+    }
+    else {
+        jQuery('#edit-field-desk-und').attr('disabled', false).trigger("chosen:updated");
+        jQuery('#edit-cd-field-old-stories').attr('disabled', true).trigger("chosen:updated");
+    }
+    jQuery('#edit-cd-field-old-stories-check').click(function(e) {
+        if (jQuery(this).is(':checked')) {
+            jQuery('#edit-field-desk-und').val('_none');
+            jQuery('#edit-field-desk-und').attr('disabled', true).trigger("chosen:updated");
+            jQuery('#edit-cd-field-old-stories').attr('disabled', false).trigger("chosen:updated");
+        }
+        else {
+            jQuery('#edit-cd-field-old-stories').val('_none');
+            jQuery('#edit-field-desk-und').attr('disabled', false).trigger("chosen:updated");
+            jQuery('#edit-cd-field-old-stories').attr('disabled', true).trigger("chosen:updated");
+        }
+    });
+});
