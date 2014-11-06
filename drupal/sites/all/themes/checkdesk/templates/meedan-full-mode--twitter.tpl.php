10a395a144f993ecab31e96b8a179d6055e9f71c
designbysoil <designbysoil@gmail.com>
2014-11-06 22:47:35 +0000

    Fixing rendering of Twitter reports with only photos.

--- drupal/sites/all/themes/checkdesk/templates/meedan-full-mode--twitter.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/meedan-full-mode--twitter.tpl.php
@@ -1,13 +1,25 @@
 <div class="media-holder">
-  <div class="media">
-    <div class="tweet-holder">
-      <?php if (isset($embed->html)) : ?>
+  <?php if (isset($embed->html)) : ?>
+    <div class="media">
+      <div class="tweet-holder">
         <?php print $embed->html; ?>
-      <?php else: ?>
-        <?php print theme('image', array('path' => $embed->thumbnail_url)); ?>
-      <?php endif; ?>
+      </div>  
     </div>
-  </div>
+  <?php elseif($embed->type == 'photo') : ?>
+    <div class="media">
+      <div class="img-holder">
+        <?php print theme('image', array('path' => $embed->url)); ?>
+      </div>
+    </div>
+    <div class="media-content">
+      <span class="title"><?php print l($node->title, 'node/' . $node->nid , array('html' => TRUE)); ?></span>
+      <?php if(isset($media_description)) : ?><span class="description expandable"><?php print $media_description; ?></span><?php endif; ?>
+      <?php if(isset($author_name)) : ?><span class="author"><?php print $author_name ?></span><?php endif; ?>
+      <span>
+        <?php if(isset($favicon_link)) : ?><span class="provider-icon"><?php print $favicon_link ?></span><?php endif; ?> <span class="ts"><?php print $media_creation_info; ?></span>
+      </span>
+    </div>
+  <?php endif; ?>
   <?php if(isset($media_description)) : ?>
     <div class="media-content">
       <span class="description expandable"><?php print $media_description; ?></span>
