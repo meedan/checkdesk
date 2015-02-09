524b318814c991251c8d56cf3bdda248ec3a2f6f
Shady Samir <shadysamirsobhi@gmail.com>
2015-02-09 02:47:50 +0200

    Fix: #3477: story_update.php looked for /sites Line 5 of story_update.php originally looked for ../../../../../sites unlike the other files in last commit. Now it's following same scheme

--- drupal/sites/all/modules/custom/checkdesk_core/autorefresh/story_update.php
+++ drupal/sites/all/modules/custom/checkdesk_core/autorefresh/story_update.php
@@ -3,10 +3,10 @@ header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past (HTTP 1.0)
 $server_name = $_SERVER['HTTP_HOST'];
 
-if (file_exists("../../../../../../{$server_name}/settings.php")) {
-  require("../../../../../../{$server_name}/settings.php");
+if (file_exists("../../../../../{$server_name}/settings.php")) {
+  require("../../../../../{$server_name}/settings.php");
 } else {
-  require("../../../../../../default/settings.php");
+  require("../../../../../default/settings.php");
 }
 
 // Connect to the database
