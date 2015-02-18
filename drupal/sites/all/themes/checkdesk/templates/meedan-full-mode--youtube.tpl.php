a06427b881aeac5521e9e3366d7c7c3e83e22b4e
Sawy <melsawy.be7eri@gmail.com>
2015-02-18 16:34:09 +0200

    Fix php warning

--- drupal/sites/all/themes/checkdesk/templates/meedan-full-mode--youtube.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/meedan-full-mode--youtube.tpl.php
@@ -1,14 +1,23 @@
 <div class="media">
   <div class="video-holder media-16by9">
-    <?php print $embed->html; ?>
+    <?php if(isset($embed->html)) : ?>
+        <?php print $embed->html; ?>
+    <?php endif; ?>
   </div>
 </div>
 <div class="media-content">
   <span class="title"><?php print l($node->title, 'node/' . $node->nid , array('html' => TRUE)); ?></span>
-  <?php if(isset($media_description)) : ?><span class="description expandable"><?php print $media_description; ?></span><?php endif; ?>
-  <?php if(isset($author_name)) : ?><span class="author"><?php print $author_name ?></span><?php endif; ?>
+  <?php if(isset($media_description)) : ?>
+    <span class="description expandable"><?php print $media_description; ?></span>
+  <?php endif; ?>
+  <?php if(isset($author_name)) : ?>
+    <span class="author"><?php print $author_name ?></span>
+  <?php endif; ?>
   <span>
-    <?php if(isset($favicon_link)) : ?><span class="provider-icon"><?php print $favicon_link ?></span><?php endif; ?> <span class="ts"><?php print $media_creation_info; ?></span>
+    <?php if(isset($favicon_link)) : ?>
+        <span class="provider-icon"><?php print $favicon_link ?></span>
+    <?php endif; ?>
+        <span class="ts"><?php print $media_creation_info; ?></span>
   </span>
 </div>
 <?php if(isset($report_status['status'])) : ?>
