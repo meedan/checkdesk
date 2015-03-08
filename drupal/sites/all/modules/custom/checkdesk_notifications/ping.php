c20e9e2a192b4102bd8d8f6ef61f56c8a03dc567
Sawy <melsawy.be7eri@gmail.com>
2015-03-08 14:18:10 +0200

    Ticket #3576 - new story notification

--- drupal/sites/all/modules/custom/checkdesk_notifications/ping.php
+++ drupal/sites/all/modules/custom/checkdesk_notifications/ping.php
@@ -74,6 +74,8 @@ if ($is_journalist) {
     $query .= "(ha.message_id = 'checkdesk_reply_to_comment' AND c.uid = $uid) OR ";
   if (should_notify($data, 'site_new_user'))
     $query .= "(ha.message_id = 'checkdesk_new_user') OR ";
+  if (should_notify($data, 'site_new_story'))
+    $query .= "(ha.message_id = 'checkdesk_add_story') OR ";
 } else {
   if (should_notify($data, 'site_reply_to_comment'))
     $query .= "(ha.message_id = 'checkdesk_reply_to_comment' AND c.uid = $uid) OR ";
