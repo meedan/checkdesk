80180d8e8435ae21b600710defde7de1e33549be
designbysoil <designbysoil@gmail.com>
2015-03-19 03:31:04 +0000

    Changes in the search results helper text.

--- drupal/sites/all/themes/checkdesk/templates/views/checkdesk_search/views-view-unformatted--checkdesk-search--attachment.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/views/checkdesk_search/views-view-unformatted--checkdesk-search--attachment.tpl.php
@@ -6,15 +6,30 @@
  *
  * @ingroup views_templates
  */
+	$total = count($rows) - 1;
 ?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <ul class="page-results">
 	<?php foreach ($rows as $id => $row): ?>
-  	<li<?php if ($classes_array[$id]) { print ' class="result ' . $classes_array[$id] .'"';  } ?>>
-    	<?php print $row; ?>
-  	</li>
+		<?php if($total > 1 && $id != $total) : ?>
+	  	<li<?php if ($classes_array[$id]) { print ' class="result ' . $classes_array[$id] .'"';  } ?>>
+	    	<?php print $row . t(','); ?>
+	  	</li>
+	  <?php elseif($total == 1 && $id == 0) : ?>
+	  	<li<?php if ($classes_array[$id]) { print ' class="result ' . $classes_array[$id] .'"';  } ?>>
+	    	<?php print $row . t(' and'); ?>
+	  	</li>
+	 	<?php elseif($id == $total) : ?>
+	  	<li<?php if ($classes_array[$id]) { print ' class="result ' . $classes_array[$id] .'"';  } ?>>
+	    	<?php print $row . t(' found'); ?>
+	  	</li>
+	  <?php else : ?>
+	  	<li<?php if ($classes_array[$id]) { print ' class="result ' . $classes_array[$id] .'"';  } ?>>
+	    	<?php print $row; ?>
+	  	</li>
+		<?php endif; ?>
 	<?php endforeach; ?>
 </ul>
 <div class="search-reset-filters">
