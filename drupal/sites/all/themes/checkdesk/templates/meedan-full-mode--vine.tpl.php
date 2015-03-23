b753f379b216c2da64e97172e8f145611574d8b7
designbysoil <designbysoil@gmail.com>
2015-03-23 20:52:22 +0000

    Fixing vine and amazon embeds.

--- drupal/sites/all/themes/checkdesk/templates/meedan-full-mode--vine.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/meedan-full-mode--vine.tpl.php
@@ -1,22 +1,18 @@
 <div class="media">
   <?php if(isset($embed->html)) : ?>
-    <div class="inline-holder inline-amazon-holder amazon-holder">
+    <div class="re-holder media-1by1">
       <?php print $embed->html; ?>
+    </div>
+  <?php elseif (isset($full_image)) : ?>
+    <div class="img-holder">
+      <?php print $full_image; ?>
     </div>
   <?php endif ;?>
 </div>
 <div class="media-content">
   <span class="title"><?php print l($node->title, 'node/' . $node->nid , array('html' => TRUE)); ?></span>
-  <?php if(isset($media_description)) : ?>
-    <span class="description expandable"><?php print $media_description; ?></span>
-  <?php elseif (isset($embed->description)) : ?>
-     <span class="description expandable"><?php print $embed->description; ?></span>
-  <?php endif; ?>
-  <?php if(isset($author_name)) : ?>
-    <span class="author"><?php print $author_name ?></span>
-  <?php elseif(isset($provider_name)) : ?>
-    <span class="provider"><?php print $provider_name ?></span>
-  <?php endif; ?>
+  <?php if(isset($media_description)) : ?><span class="description expandable"><?php print $media_description; ?></span><?php endif; ?>
+  <?php if(isset($author_name)) : ?><span class="author"><?php print $author_name ?></span><?php endif; ?>
   <span>
     <?php if(isset($favicon_link)) : ?><span class="provider-icon"><?php print $favicon_link ?></span><?php endif; ?> <span class="ts"><?php print $media_creation_info; ?></span>
   </span>
