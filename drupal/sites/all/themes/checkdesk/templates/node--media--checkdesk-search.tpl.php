36f64a842c37845a7e37f67f082ab1d84f2e4e5b
Sawy <melsawy.be7eri@gmail.com>
2015-03-23 13:59:08 +0200

    Ticket #3551

--- drupal/sites/all/themes/checkdesk/templates/node--media--checkdesk-search.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/node--media--checkdesk-search.tpl.php
@@ -1,13 +1,10 @@
 <section id="node-<?php print $node->nid; ?>" class="node-<?php print $node->nid; ?> <?php print $classes; ?>"<?php print $attributes; ?>>
-	<div class="item-content-wrapper<?php if (isset($status_class)) { print ' ' . $status_class; } ?>">
+
+   <div class="item-content-wrapper<?php if (isset($status_class)) { print ' ' . $status_class; } ?>">
 	  <span class="item-content">
 	    <?php print render($content['field_link']); ?>
 	  </span> <!-- /item-content -->
 	</div> <!-- /content-wrapper -->
-
-    <?php if (isset($content['body'])) : ?>
-      <?php print render($content['body']); ?>
-    <?php endif; ?>
-  
    <?php print $report_activity; ?>
+
 </section>
