5673b510d67d4e50f96e04a640cb7617ea9fe4f4
infojunkie <karim.ratib@gmail.com>
2015-04-02 17:21:04 -0700

    Better logo handling

--- drupal/sites/all/themes/checkdesk/theme-settings.php
+++ drupal/sites/all/themes/checkdesk/theme-settings.php
@@ -10,63 +10,40 @@ function checkdesk_form_system_theme_settings_alter(&$form, &$form_state) {
     '#type' => 'fieldset',
     '#title' => t('Header'),
   );
-
   $path = theme_get_setting('header_image_path');
-
   $form['header']['header_image_path'] = array(
     '#type' => 'value',
     '#value' => $path,
   );
-
-  if (file_uri_scheme($path) == 'public') {
-    $path = file_uri_target($path);
-  }
-
   $form['header']['header_image_upload'] = array(
     '#type' => 'file',
     '#title' => t('Choose a logo that will appear above your stories:'),
   );
-
+  if (file_uri_scheme($path) === 'public') {
+    $path = file_uri_target($path);
+  }
   $form['header']['header_image_preview'] = array(
-    '#markup' => (empty($path) ? '' : '<img src="' . image_style_url('thumbnail', $path) . '" />'),
+    '#markup' => (empty($path) ? '' : '<img src="' . image_style_url('partner_logo', $path) . '" />'),
   );
 
-  $form['header']['header_image_enabled'] = array(
-    '#type' => 'checkbox',
-    '#title' => t('Enable custom header image'),
-    '#default_value' => theme_get_setting('header_image_enabled'),
+  $form['footer'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Footer'),
   );
-
-  $form['header']['header_image_position'] = array(
-    '#type' => 'radios',
-    '#title' => t('Choose a position for the image:'),
-    '#default_value' => theme_get_setting('header_image_position'),
-    '#options' => array('center' => t('Center'), 'right' => t('Right'), 'left' => t('Left')),
-  );
-
-  $path = theme_get_setting('header_bg_path');
-
-  $form['header']['header_bg_path'] = array(
+  $path = theme_get_setting('footer_image_path');
+  $form['footer']['footer_image_path'] = array(
     '#type' => 'value',
     '#value' => $path,
   );
-
-  if (file_uri_scheme($path) == 'public') {
-    $path = file_uri_target($path);
-  }
-
-  $form['header']['header_bg_upload'] = array(
-    '#type' => 'file',
-    '#title' => t('Choose a background image that will appear on the header:'),
-  );
-
-  $form['header']['header_bg_preview'] = array(
-    '#markup' => (empty($path) ? '' : '<img src="' . image_style_url('thumbnail', $path) . '" />'),
-  );
-
   $form['footer']['footer_image_upload'] = array(
     '#type' => 'file',
     '#title' => t('Choose a logo that will appear in the footer:'),
+  );
+  if (file_uri_scheme($path) === 'public') {
+    $path = file_uri_target($path);
+  }
+  $form['footer']['footer_image_preview'] = array(
+    '#markup' => (empty($path) ? '' : '<img src="' . image_style_url('footer_partner_logo', $path) . '" />'),
   );
 
   $form['#submit'][] = 'checkdesk_settings_submit';
@@ -87,21 +64,12 @@ function checkdesk_settings_submit($form, &$form_state) {
     $_POST['header_image_path'] = $form_state['values']['header_image_path'] = $file->destination;
   }
 
-  if ($file = file_save_upload('header_bg_upload', array('file_validate_is_image' => array()), $filepath, FILE_EXISTS_REPLACE)) {
-    // Make the file permanent
-    $file->status = 1;
-    file_save($file);
-    $_POST['header_bg_path'] = $form_state['values']['header_bg_path'] = $file->destination;
-  }
-
   if ($file = file_save_upload('footer_image_upload', array('file_validate_is_image' => array()), $filepath, FILE_EXISTS_REPLACE)) {
     // Make the file permanent
     $file->status = 1;
     file_save($file);
     $_POST['footer_image_path'] = $form_state['values']['footer_image_path'] = $file->destination;
   }
-
-
 }
 
 ?>
