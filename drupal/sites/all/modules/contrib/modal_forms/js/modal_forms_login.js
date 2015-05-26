75f541ada9de11f7628a51d7e46fe0a67d947919
Sawy <melsawy.be7eri@gmail.com>
2015-05-26 23:30:19 +0300

    Use medium modal for login forms

--- drupal/sites/all/modules/contrib/modal_forms/js/modal_forms_login.js
+++ drupal/sites/all/modules/contrib/modal_forms/js/modal_forms_login.js
@@ -4,7 +4,7 @@ Drupal.behaviors.initModalFormsLogin = {
   attach: function (context, settings) {
     $("a[href*='/user/login'], a[href*='?q=user/login']", context).once('init-modal-forms-login', function () {
       this.href = this.href.replace(/user\/login/,'modal_forms/nojs/login');
-    }).addClass('ctools-use-modal ctools-modal-modal-popup-small');
+    }).addClass('ctools-use-modal ctools-modal-modal-popup-medium');
   }
 };
 
--- drupal/sites/all/modules/contrib/modal_forms/js/modal_forms_password.js
+++ drupal/sites/all/modules/contrib/modal_forms/js/modal_forms_password.js
@@ -4,7 +4,7 @@ Drupal.behaviors.initModalFormsPassword = {
   attach: function (context, settings) {
     $("a[href*='/user/password'], a[href*='?q=user/password']", context).once('init-modal-forms-password', function () {
       this.href = this.href.replace(/user\/password/,'modal_forms/nojs/password');
-    }).addClass('ctools-use-modal ctools-modal-modal-popup-small');
+    }).addClass('ctools-use-modal ctools-modal-modal-popup-medium');
   }
 };
 
