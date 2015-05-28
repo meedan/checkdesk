fe0a874079d7713d64774329dc32a0859a9ab3b9
designbysoil <designbysoil@gmail.com>
2015-05-28 07:50:18 +0000

    Ticket #3742: Theming sign in/up screen.

--- drupal/sites/all/modules/custom/checkdesk_core/theme/checkdesk_sign_in_up.tpl.php
+++ drupal/sites/all/modules/custom/checkdesk_core/theme/checkdesk_sign_in_up.tpl.php
@@ -1,30 +1,19 @@
-<div>
- <div>
-   <h1 class="title"><?php print t('Checkdesk'); ?></h1>
-   <div><?php print t('Sign in or create an account'); ?></div>
- </div>
+<h3 class="title"><?php print t('Checkdesk'); ?></h3>
+<div class="description"><?php print t('Sign in or create an account'); ?></div>
+<div class="actions">
   <?php if(isset($links['twitter'])) : ?>
-    <div class="cd-twitter">
-      <?php print $links['twitter'] ?>
-    </div>
+    <?php print $links['twitter'] ?>
   <?php endif; ?>
 
   <?php if(isset($links['facebook'])) : ?>
-    <div class="cd-facebook">
-      <?php print $links['facebook'] ?>
-    </div>
+    <?php print $links['facebook'] ?>
   <?php endif; ?>
 
-  <div class="cd-login">
-    <?php print $links['login'] ?>
-  </div>
+  <?php print $links['login'] ?>
 
   <div class="or">
     <span><?php print t('or'); ?></span>
   </div>
 
-  <div class="cd-register">
-    <?php print $links['register'] ?>
-  </div>
-
+  <?php print $links['register'] ?>
 </div>
