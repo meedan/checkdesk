47dd349e8b5cedab04e83aeb40c1ebf47679864b
designbysoil <designbysoil@gmail.com>
2015-05-28 21:38:21 +0000

    Ticket #3742: Adding close button icon.

--- drupal/sites/all/modules/custom/checkdesk_core/theme/checkdesk_sign_in_up.tpl.php
+++ drupal/sites/all/modules/custom/checkdesk_core/theme/checkdesk_sign_in_up.tpl.php
@@ -1,19 +1,21 @@
-<h3 class="title"><?php print t('Checkdesk'); ?></h3>
-<div class="description"><?php print t('Sign in or create an account'); ?></div>
-<div class="actions">
-  <?php if(isset($links['twitter'])) : ?>
-    <?php print $links['twitter'] ?>
-  <?php endif; ?>
+<div class="modal-dialog-signin">
+  <h3 class="title"><?php print t('Checkdesk'); ?></h3>
+  <div class="description"><?php print t('Sign in or create an account'); ?></div>
+  <div class="actions">
+    <?php if(isset($links['twitter'])) : ?>
+      <?php print $links['twitter'] ?>
+    <?php endif; ?>
 
-  <?php if(isset($links['facebook'])) : ?>
-    <?php print $links['facebook'] ?>
-  <?php endif; ?>
+    <?php if(isset($links['facebook'])) : ?>
+      <?php print $links['facebook'] ?>
+    <?php endif; ?>
 
-  <?php print $links['login'] ?>
+    <?php print $links['login'] ?>
 
-  <div class="or">
-    <span><?php print t('or'); ?></span>
+    <div class="or">
+      <span><?php print t('or'); ?></span>
+    </div>
+
+    <?php print $links['register'] ?>
   </div>
-
-  <?php print $links['register'] ?>
 </div>
