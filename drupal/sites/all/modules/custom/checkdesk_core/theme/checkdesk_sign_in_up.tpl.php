53227d45f9e5f636783ca24c963ec2ef957d7970
designbysoil <designbysoil@gmail.com>
2015-06-02 00:00:08 +0000

    Ticket #3743: Theming for sign in with email form.

--- drupal/sites/all/modules/custom/checkdesk_core/theme/checkdesk_sign_in_up.tpl.php
+++ drupal/sites/all/modules/custom/checkdesk_core/theme/checkdesk_sign_in_up.tpl.php
@@ -1,7 +1,7 @@
 <div class="modal-dialog modal-dialog-signin">
-  <h3 class="title"><?php print t('Checkdesk'); ?></h3>
-  <div class="description"><?php print t('Sign in or create an account'); ?></div>
-  <div class="actions">
+  <h3 class="modal-dialog-title"><?php print t('Checkdesk'); ?></h3>
+  <div class="modal-dialog-description"><?php print t('Sign in or create an account'); ?></div>
+  <div class="modal-dialog-actions">
     <?php if(isset($links['twitter'])) : ?>
       <?php print $links['twitter'] ?>
     <?php endif; ?>
