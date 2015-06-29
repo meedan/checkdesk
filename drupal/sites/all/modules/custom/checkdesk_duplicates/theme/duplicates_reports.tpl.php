ed83d567e7e806e5ec9aa4879961fdab9602da28
Sawy <melsawy.be7eri@gmail.com>
2015-06-29 13:32:39 +0200

    Ticket #3642

--- drupal/sites/all/modules/custom/checkdesk_duplicates/theme/duplicates_reports.tpl.php
+++ drupal/sites/all/modules/custom/checkdesk_duplicates/theme/duplicates_reports.tpl.php
@@ -5,7 +5,7 @@
  * Checkdesk duplicates reports template.
  *
  * Variables available:
- * - $reports: associative array with report nid and report data in format [User1 added this link to story X]
+ * - $reports: associative array with report nid and report data as associative array (user and story)
  * - $duplicate_report_nid: The report nid for the one exists in same story
  *
  */
@@ -16,11 +16,17 @@
   <div class="activity-item item duplicates-reports-row <?php print ($nid == $duplicate_report_nid) ? 'cd-report-existing' : '' ?>">
     <div class="activity-item-wrapper item-wrapper">
       <div class="activity-item-message item-message">
-        <?php print $data; ?>
+        <?php print $data['author']; ?>
+        <?php if (!empty($data['story'])) : ?>
+          <?php print t('added this link to'); ?>
+          <?php print $data['story']; ?>
+        <?php endif; ?>
+        <time class="timestamp"><?php print $data['created_at']; ?></time>
       </div>
       <div class="activity-item-content-wrapper item-content-wrapper cd-report-link">
         <span class="activity-item-content item-content">
           <?php print l(t('View the link'), 'node/' . $nid, array('attributes' => array('target'=>'_blank'))); ?>
+          <?php print l(t('Cancel'), '#'); ?>
         </span>
       </div>
     </div>
