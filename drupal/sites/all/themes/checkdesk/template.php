191859d5581a1878dcb2b958d18125362580ef24
infojunkie <karim.ratib@gmail.com>
2015-07-02 16:19:37 -0700

    Remove automatically-generated site slogan #3942

--- drupal/sites/all/themes/checkdesk/template.php
+++ drupal/sites/all/themes/checkdesk/template.php
@@ -206,16 +206,9 @@ function checkdesk_preprocess_region(&$variables) {
       );
       $variables['frontpage_logo'] = theme('image_style', $frontpage_logo_data);
     }
-
-    $position = theme_get_setting('header_logo_position');
-    $variables['header_logo_position'] = (empty($position) ? 'left' : $position);
-
     $bg = theme_get_setting('header_bg_path');
     $variables['header_bg'] = (empty($bg) ? '' : file_create_url($bg));
-
-    $slogan = $variables['header_slogan'] = t('A Checkdesk live blog by <a href="@partner_url" target="_blank"><span class="checkdesk-slogan-partner">@partner</span></a>', array('@partner' => variable_get_value('checkdesk_site_owner', array('language' => $language)), '@partner_url' => variable_get_value('checkdesk_site_owner_url', array('language' => $language))));
-    $variables['header_slogan'] = (empty($slogan) ? '' : $slogan);
-    $variables['header_slogan_position'] = ((!empty($position) && in_array($position, array('center', 'right'))) ? 'left' : 'right');
+    $variables['header_slogan'] = variable_get('site_slogan', '');
   }
 
   if ($variables['region'] == 'footer') {
