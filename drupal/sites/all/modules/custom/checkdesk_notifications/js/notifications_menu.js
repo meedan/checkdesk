1ffef55d91e0a4a0138be89b6a2492f27310e0e7
infojunkie <karim.ratib@gmail.com>
2015-07-06 23:43:33 -0700

    Play optional sound on notification

--- drupal/sites/all/modules/custom/checkdesk_notifications/js/notifications_menu.js
+++ drupal/sites/all/modules/custom/checkdesk_notifications/js/notifications_menu.js
@@ -45,20 +45,37 @@ Drupal.behaviors.meedan_notifications_load_more = {
 
 Drupal.behaviors.alert_new_notifications = {
   attach: function (context, settings) {
+    var soundNewItem = false;
+    if (Drupal.settings.alert_new_notifications.play_sound) {
+      soundManager.setup({
+        url: Drupal.settings.basePath + 'sites/all/libraries/soundmanager2/swf/',
+        preferFlash: false,
+        onready: function() {
+          // @see http://www.freesound.org/people/GabrielAraujo/sounds/242502/
+          soundNewItem = soundManager.createSound({
+            id: 'newItem',
+            url: Drupal.settings.basePath + 'sites/all/modules/custom/checkdesk_notifications/sounds/notification.mp3'
+          });
+        },
+        ontimeout: function() {
+          console.log('Error starting SoundManager 2.');
+        }
+      });
+    }
+
     var block = $('#my-notifications'),
         counter = $('#my-notifications-menu-link').find('.notifications-count');
     if (counter.html() !== '') {
       document.title = pageTitle + ' (' + counter.html() + ')';
     }
-    block.unbind('autorefresh_update');
-    block.bind('autorefresh_update', function(e, nid) {
+    block.unbind('autorefresh_update').bind('autorefresh_update', function(e, nid) {
       Drupal.behaviors.meedan_notifications_load_more.attach();
     });
-    block.unbind('autorefresh_ping');
-    block.bind('autorefresh_ping', function(e, count) {
+    block.unbind('autorefresh_ping').bind('autorefresh_ping', function(e, count) {
       var total = (counter.html() === '') ? count : (parseInt(counter.html(), 10) + parseInt(count, 10));
       counter.addClass('badge').html(total);
       document.title = pageTitle + ' (' + total + ')';
+      if (soundNewItem) soundNewItem.play();
     });
   }
 };
