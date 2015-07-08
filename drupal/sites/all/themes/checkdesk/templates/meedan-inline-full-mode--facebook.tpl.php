b28722e6f496489000e402c97b2c7cbdd7ab74c4
designbysoil <designbysoil@gmail.com>
2015-07-08 18:43:51 +0000

    Ticket #3971: Showing Facebook report description.

--- drupal/sites/all/themes/checkdesk/templates/meedan-inline-full-mode--facebook.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/meedan-inline-full-mode--facebook.tpl.php
@@ -10,11 +10,16 @@
       </div>
     <?php endif ;?>
   </div>
-  <?php if(isset($media_description)) : ?>
-    <div class="media-content">
-      <span class="description expandable"><?php print $media_description; ?></span>
-    </div>
-  <?php endif; ?>
+  <div class="media-content">
+    <?php if (isset($full_image) || $embed->type == 'video') : ?>
+      <span class="title"><?php print l($node->title, 'node/' . $node->nid , array('html' => TRUE)); ?></span>
+    <?php endif; ?>
+    <?php if(isset($media_description)) : ?><span class="description expandable"><?php print $media_description; ?></span><?php endif; ?>
+    <?php if(isset($author_name)) : ?><span class="author"><?php print $author_name ?></span><?php endif; ?>
+    <span>
+      <?php if(isset($favicon_link)) : ?><span class="provider-icon"><?php print $favicon_link ?></span><?php endif; ?> <span class="ts"><?php print $media_creation_info; ?></span>
+    </span>
+  </div>
   <?php if(isset($report_status['status'])) : ?>
     <span class="inline-attachment-status media-status">
       <?php print $report_status['status']; ?>
