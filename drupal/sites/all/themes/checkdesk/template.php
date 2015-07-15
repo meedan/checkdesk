59c22009a013b92a2d10378d155b6bb1ff7744cc
designbysoil <designbysoil@gmail.com>
2015-07-15 23:58:08 +0000

    Ticket #3996: Removing logos from footer. Part 2.

--- drupal/sites/all/themes/checkdesk/template.php
+++ drupal/sites/all/themes/checkdesk/template.php
@@ -238,7 +238,6 @@ function checkdesk_preprocess_region(&$variables) {
   if ($variables['region'] == 'footer') {
     // define custom header settings
     $partner_name = variable_get('site_name', 'Drupal');
-    dsm($partner_name);
     if (!empty($partner_name)) {
       $variables['partner_name'] = $partner_name;
       $variables['partner_url'] = variable_get_value('checkdesk_site_owner_url', array('language' => $language));
