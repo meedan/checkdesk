eb408c04c00cf9d8780f0e36d8659321a49c7d35
designbysoil <designbysoil@gmail.com>
2015-07-16 00:17:01 +0000

    Ticket #3996: Removing logos from footer. Part 3.

--- drupal/sites/all/themes/checkdesk/theme-settings.php
+++ drupal/sites/all/themes/checkdesk/theme-settings.php
@@ -66,27 +66,6 @@ function checkdesk_form_system_theme_settings_alter(&$form, &$form_state) {
     '#markup' => (empty($path) ? '' : '<img src="' . image_style_url('activity_avatar', $path) . '" />'),
   );
 
-  // footer logo
-  $form['footer'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Footer'),
-  );
-  $path = theme_get_setting('footer_image_path');
-  $form['footer']['footer_image_path'] = array(
-    '#type' => 'value',
-    '#value' => $path,
-  );
-  $form['footer']['footer_image_upload'] = array(
-    '#type' => 'file',
-    '#title' => t('Choose a logo that will appear in the footer:'),
-  );
-  if (file_uri_scheme($path) === 'public') {
-    $path = file_uri_target($path);
-  }
-  $form['footer']['footer_image_preview'] = array(
-    '#markup' => (empty($path) ? '' : '<img src="' . image_style_url('footer_partner_logo', $path) . '" />'),
-  );
-
   $form['#submit'][] = 'checkdesk_settings_submit';
 }
 
@@ -117,13 +96,6 @@ function checkdesk_settings_submit($form, &$form_state) {
     $file->status = 1;
     file_save($file);
     $_POST['badge_logo_path'] = $form_state['values']['badge_logo_path'] = $file->destination;
-  }
-
-  if ($file = file_save_upload('footer_image_upload', array('file_validate_is_image' => array()), $filepath, FILE_EXISTS_REPLACE)) {
-    // Make the file permanent
-    $file->status = 1;
-    file_save($file);
-    $_POST['footer_image_path'] = $form_state['values']['footer_image_path'] = $file->destination;
   }
 }
 
