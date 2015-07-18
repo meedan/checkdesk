ad0efd4f671cf073a385aeb42577afccca622e26
infojunkie <karim.ratib@gmail.com>
2015-07-18 01:07:50 -0700

    Scroll to active item position #3931

--- drupal/sites/all/modules/custom/checkdesk_core/js/checkdesk_search.js
+++ drupal/sites/all/modules/custom/checkdesk_core/js/checkdesk_search.js
@@ -4,16 +4,10 @@
 
   Drupal.behaviors.searchPage = {
     attach: function (context, settings) {
-      // set filter value to "All" if it's not already set.
-      $('.filter-list .bef-select-as-links').each(function() {
-        if ($('.form-type-bef-link a[class="active"]', this).length === 0) {
-          $('.form-type-bef-link[id$="-all"] a', this).addClass('active');
-        }
+      // auto-submit form after user click/unclick unassigned reports
+      $( "#edit-report-unassigned" ).click(function() {
+          $('#views-exposed-form-checkdesk-search-page').submit();
       });
-
-      // collapse filter group whose active value is "All" and open others.
-      $('.filter-list .form-type-bef-link[id!="edit-type-all"][id$="-all"] a[class="active"]').parents('.bef-select-as-links').hide();
-      $('.filter-list .form-type-bef-link:not([id$="-all"]) a[class="active"]').parents('.form-type-select, .views-exposed-widget').addClass('open');
 
       // filter group collapse/expand
       $('.filter-list > .views-exposed-widget label').unbind('click').click(function(event) {
@@ -27,10 +21,22 @@
         return false;
       });
 
-      // auto-submit form after user click/unclick unassigned reports
-      $( "#edit-report-unassigned" ).click(function() {
-          $('#views-exposed-form-checkdesk-search-page').submit();
+      // set filter value to "All" if it's not already set.
+      $('.filter-list .bef-select-as-links').each(function() {
+        if ($('.form-type-bef-link a[class="active"]', this).length === 0) {
+          $('.form-type-bef-link[id$="-all"] a', this).addClass('active');
+        }
       });
+
+      // collapse filter group whose active value is "All" and open others.
+      $('.filter-list .form-type-bef-link[id!="edit-type-all"][id$="-all"] a[class="active"]').parents('.bef-select-as-links').hide();
+      $('.filter-list .form-type-bef-link:not([id$="-all"]) a[class="active"]').parents('.form-type-select, .views-exposed-widget').addClass('open');
+
+      // scroll to active elements inside filters.
+      // @see http://stackoverflow.com/questions/2346011/jquery-scroll-to-an-element-within-an-overflowed-div
+      var $parentDiv = $('.filter-list .search-list'),
+          $innerListItem = $('.form-type-bef-link a[class="active"]', $parentDiv);
+      $parentDiv.scrollTop($parentDiv.scrollTop() + $innerListItem.position().top);
     }
   };
 
