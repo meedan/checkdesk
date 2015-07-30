fa4167651b72b3d91a89b5faca431416f0213458
designbysoil <designbysoil@gmail.com>
2015-07-30 19:41:33 +0000

    Improvements to the container row model.

--- drupal/sites/all/themes/checkdesk/templates/views/recent_stories_by_tag/views-view-unformatted--recent-stories-by-tag.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/views/recent_stories_by_tag/views-view-unformatted--recent-stories-by-tag.tpl.php
@@ -1,6 +1,6 @@
-<ul class="cd-slice l-row u-unstyled">
+<ul class="cd-slice l-row l-row-cols-<?php print count($rows); ?> u-unstyled">
 	<?php foreach ($rows as $id => $row): ?>
-  	<li class="cd-slice-item l-row-item-span-1<?php if ($classes_array[$id]) { print ' ' . $classes_array[$id];  } ?>">
+  	<li class="cd-slice-item l-row-item l-row-item-span-1<?php if ($classes_array[$id]) { print ' ' . $classes_array[$id];  } ?>">
     	<?php print $row; ?>
   	</li>
 	<?php endforeach; ?>
