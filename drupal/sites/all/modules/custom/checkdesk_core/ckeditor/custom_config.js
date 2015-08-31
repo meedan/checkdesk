85144e23d99a833595d0f4fd1034558c5525d791
Sawy <melsawy.be7eri@gmail.com>
2015-08-31 21:04:21 +0200

    Ticket #4048

--- drupal/sites/all/modules/custom/checkdesk_core/ckeditor/custom_config.js
+++ drupal/sites/all/modules/custom/checkdesk_core/ckeditor/custom_config.js
@@ -13,5 +13,13 @@
         instance.dataProcessor.writer.setRules('p', rules);
         instance.dataProcessor.writer.setRules('div', rules);
         instance.dataProcessor.writer.setRules('figure', rules);
+
+        //catch ctrl+clicks on <a>'s in edit mode to open hrefs in new tab/window
+        jQuery('iframe').contents().click(function(el) {  
+          el.target.title = Drupal.t('Press Ctrl + click to open the link');
+          if(typeof el.target.href != 'undefined' && el.ctrlKey == true) {          
+            window.open(el.target.href, 'new' + el.screenX);
+          }
+        });
     });
 })();
