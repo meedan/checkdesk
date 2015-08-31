27e4341ee54789e1c4cf17b0a72e5409d9fe6840
Caio SBA <caiosba@gmail.com>
2015-08-31 01:14:13 -0300

    Ticket #4149: Bugifx

--- nodejs/rest-websocket-server.js
+++ nodejs/rest-websocket-server.js
@@ -3,33 +3,34 @@
 var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({ port: 8000 });
 
+var wsi = null;
+
 wss.on('connection', function connection(ws) {
+  wsi = ws;
+
   ws.on('message', function incoming(message) {
     console.log('WebSocket received: %s', message);
   });
+
   console.log('WebSocket server running on port 8000');
-  
-  startRESTServer(ws);
 });
 
 // REST Server
 
-var startRESTServer = function(ws) {
-  var express = require('express'),
-      app = express(),
-      bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
 
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.post('/send', function(req, res) {
-    console.log('Received:');
-    console.log(req.body);
-    ws.send(JSON.stringify(req.body));
-    res.end('{"success":true}');
-  });
+app.post('/send', function(req, res) {
+  console.log('Received:');
+  console.log(req.body);
+  wsi.send(JSON.stringify(req.body));
+  res.end('{"success":true}');
+});
 
-  var server = app.listen(8001, function() {
-    console.log('REST server listening on port 8001')
-  });
-}
+var server = app.listen(8001, function() {
+  console.log('REST server listening on port 8001')
+});
