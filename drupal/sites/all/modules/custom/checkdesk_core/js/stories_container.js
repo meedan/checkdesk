f27af4e3ce3bd5a6b6ac06ebf7f3f3443f04fd48
Sawy <melsawy.be7eri@gmail.com>
2015-09-19 11:49:05 +0200

    Adjust stories container js file

--- drupal/sites/all/modules/custom/checkdesk_core/js/stories_container.js
+++ drupal/sites/all/modules/custom/checkdesk_core/js/stories_container.js
@@ -1,10 +1,9 @@
-/*jslint nomen: true, plusplus: true, todo: true, white: true, browser: true, indent: 2 */
-(function ($) {
-  'use strict';
-  jQuery('div.cd-slice-wrapper li.cd-slice-item').hover(
+jQuery(function() {
+
+    jQuery('div.cd-slice-wrapper li.cd-slice-item').hover(
       function () {
         jQuery(this).find('div.cd-item-container').toggleClass('u-faux-block-link-hover');
       }
   );
 
-}(jQuery));
+});
