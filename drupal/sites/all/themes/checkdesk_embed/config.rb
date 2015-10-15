023e5e8c383be9587838cc9b2abc46b947e4d512
infojunkie <karim.ratib@gmail.com>
2015-10-15 11:01:20 -0700

    Remove checkdesk_embed theme #4277

--- drupal/sites/all/themes/checkdesk_embed/config.rb
+++
@@ -1,8 +0,0 @@
-require "susy"
-
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/scss"
-images_dir = "assets/images"
-javascripts_dir = "assets/js"
