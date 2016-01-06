7546ab5886b34ae40934b0699dc97ec7464cef3f
Sawy <melsawy.be7eri@gmail.com>
2016-01-06 15:02:03 +0200

    Ticket #4467

--- drupal/sites/all/modules/custom/checkdesk_nodejs/js/checkdesk_nodejs.js
+++ drupal/sites/all/modules/custom/checkdesk_nodejs/js/checkdesk_nodejs.js
@@ -2,8 +2,6 @@
 
   Drupal.Nodejs.callbacks.reportReactor = {
     callback: function (message) {
-      console.log("Let's update report - reactor replacement");
-      console.log(message);
       Drupal.nodejs_ajax.runCommands(message);
 
     }
