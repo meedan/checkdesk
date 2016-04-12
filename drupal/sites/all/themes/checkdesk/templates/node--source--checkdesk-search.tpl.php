10a0babd7624ee62a75cababd82dc51f3de64a17
Sawy <melsawy.be7eri@gmail.com>
2016-04-12 09:43:45 +0200

    Ticket #4630

--- drupal/sites/all/themes/checkdesk/templates/node--source--checkdesk-search.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/node--source--checkdesk-search.tpl.php
@@ -3,16 +3,24 @@
     <div class="item-wrapper">
       <section class="source-holder item-content-wrapper">
 
-        <div><?php print render($content['field_username']); ?></div>
-        <div><?php print render($content['field_image']); ?></div>
-        <div><?php print render($content['body']); ?></div>
-        <div><?php print render($content['field_source_url']); ?></div>
-        <div><?php print render($content['field_source_status']); ?></div>
-        <?php if (count($source_metadata)) : ?>
-          <?php foreach($source_metadata as $k => $metadata) : ?>
-            <div> <?php print render($content[$metadata]); ?> </div>
-          <?php endforeach; ?>
-        <?php endif; ?>
+        <div class="source-avatar"><?php print render($content['field_image']); ?></div>
+        <div class="source-content">
+          <span class="title"><?php print l($pender->data->title, 'node/' . $node->nid , array('html' => TRUE)); ?></span>
+          <span class="username"><?php print $username_link; ?></span>
+          <span class="description expandable"><?php print render($content['body']); ?></span>
+        </div>
+        <div class="source-metadata">
+          <?php if (count($source_metadata)) : ?>
+            <?php foreach($source_metadata as $k => $metadata) : ?>
+              <div class="source-metadata-item"><?php print render($content[$metadata]); ?></div>
+            <?php endforeach; ?>
+          <?php endif; ?>
+        </div>
+        
+        <?php if(isset($references)) : ?>
+          <?php print $references; ?>
+        <?php endif ?>
+
       </section>
 
       <?php print $source_activity; ?>
@@ -23,8 +31,7 @@
     <!-- tag list -->
     <?php if(isset($content['field_source_tags'])) : ?>
       <?php print render($content['field_source_tags']); ?>
-    <?php endif ?>
-    
-     </aside>
+    <?php endif ?>  
+  </aside>
 
 </section>
