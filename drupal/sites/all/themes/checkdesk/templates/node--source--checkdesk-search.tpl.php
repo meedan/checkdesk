8444cc55e886209aecace60b40df98214c0c810a
Abdul Rehman Khawar <designbysoil@gmail.com>
2016-04-14 00:16:22 +0000

    Ticket #4630: Source search template.

--- drupal/sites/all/themes/checkdesk/templates/node--source--checkdesk-search.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/node--source--checkdesk-search.tpl.php
@@ -1,6 +1,7 @@
 <section id="node-<?php print $node->nid; ?>" class="source-row-container node-<?php print $node->nid; ?> <?php print $classes; ?>"<?php print $attributes; ?>>
     <div class="item-content-wrapper<?php if (isset($status_class)) { print ' ' . $status_class; } ?>">
       <span class="item-content">
+        <div class="media-holder media-inline-holder source-holder source-inline-holder<?php if (isset($media_type_class)) { print ' ' . $media_type_class; } ?>">
           <div class="media">
             <div class="inline-holder inline-img-thumb-holder">
               <?php print render($content['field_image']); ?>
@@ -11,6 +12,7 @@
             <span class="username"><?php print $username_link; ?></span>
             <span class="description expandable"><?php print render($content['body']); ?></span>
           </div>
+        </div> <!-- /media-holder -->
       </span> <!-- /item-content -->
     </div> <!-- /content-wrapper -->
   <?php print $source_activity; ?>
