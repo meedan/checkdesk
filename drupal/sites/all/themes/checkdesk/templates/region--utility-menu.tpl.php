5451bc23c555e30ab56e0763c86ea439ac3aa133
Abdul Rehman Khawar <designbysoil@gmail.com>
2016-04-21 21:25:33 +0000

    Ticket #4609: Language switcher for mobile.

--- drupal/sites/all/themes/checkdesk/templates/region--utility-menu.tpl.php
+++ drupal/sites/all/themes/checkdesk/templates/region--utility-menu.tpl.php
@@ -1,3 +1,6 @@
-<div id="utility-menu">
-	<?php print $content; ?>
-</div>
+<ul id="utility-menu-wrapper">
+	<li>
+		<a id="utility-menu-toggle" class="dropdown-toggle" data-toggle="dropdown"><span class="icon-globe"></span></a>
+		<div id="utility-menu"><?php print $content; ?></div>
+	</li>
+</ul>
